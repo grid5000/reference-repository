7dc3a4101a657230b7ad0534025a7ca93c905411
Cyril Constantin <g5kadmin@fgrelon1.nancy.grid5000.fr>
2009-04-20 09:09:05 +0200

    [Nancy] Added griffon cluster

--- generators/input/nancy.rb
+++ generators/input/nancy.rb
@@ -57,4 +57,53 @@ site :nancy do |site_uid|
       end
     end
   end # cluster grelon
+
+  cluster :griffon do |cluster_uid|
+    model "Carri System CS-5393B"
+    created_at Time.parse("2009-04-10 12:00 GMT").httpdate
+    92.times do |i|
+      node "#{cluster_uid}-#{i+1}" do |node_uid|
+        supported_job_types({:deploy => true, :besteffort => true, :virtual => "ivt"})
+        architecture({
+          :smp_size => 2, 
+          :smt_size => 8,
+          :platform_type => "x86_64"
+          })
+        processor({
+          :vendor => "Intel",
+          :model => "Intel Xeon",
+          :version => "L5420",
+          :clock_speed => 2.5.giga,
+          :instruction_set => "",
+          :other_description => "",
+          :cache_l1 => nil,
+          :cache_l1i => nil,
+          :cache_l1d => nil,
+          :cache_l2 => 12.MB
+        })
+        main_memory({
+          :ram_size => 16.GB(true), # bytes
+          :virtual_size => nil
+        })
+        operating_system({
+          :name => nil,
+          :release => nil,
+          :version => nil
+        })
+        storage_devices [
+          {:interface => 'SATA II', :size => 320.GB(false), :driver => "ata_piix"}
+          ]
+        network_adapters [
+          {:interface => 'Ethernet', :rate => 1.giga, :enabled => true, 
+            :switch => "sgriffon1", :network_address => "#{node_uid}.#{site_uid}.grid5000.fr", :ip => dns_lookup("#{node_uid}.#{site_uid}.grid5000.fr"),
+            :driver => "e1000e", :vendor => "intel", :version => "80003ES2LAN"},
+          {:interface => 'Ethernet', :rate => 1.giga, :enabled => false, :driver => "e1000e", :vendor => "intel", :version => "BCM5721"},
+	  {:interface => 'Infiniband 20G', :rate => 20.giga, :enabled => true,
+	    :switch => "ib_switch", :driver => "mlx4_core", :vendor => "Mellanox", :version => "MT26418" }
+          ]
+      end
+    end
+  end # cluster grelon
+
+
 end
