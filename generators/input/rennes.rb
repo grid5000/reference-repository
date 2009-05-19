7d2648eaad7dbbc6f1fdb9c0279f73d374ccd47a
Pascal Morillon <pascal.morillon@irisa.fr>
2009-05-19 09:13:16 +0200

    [rennes] cluster paravent is retired

--- generators/input/rennes.rb
+++ generators/input/rennes.rb
@@ -13,53 +13,7 @@ site :rennes do |site_uid|
     %w{etch-x64-base-1.0 etch-x64-base-1.1 etch-x64-nfs-1.0 etch-x64-nfs-1.1 etch-x64-big-1.0 etch-x64-big-1.1} +
     %w{lenny-x64-base-0.9 lenny-x64-nfs-0.9 lenny-x64-big-0.9}  ).each{|env_uid| environment env_uid, :refer_to => "grid5000/environments/#{env_uid}"}
   
-  cluster :paravent do |cluster_uid|
-    model "HP ProLiant DL145G2"
-    created_at nil
-    32.times do |i|
-      node "#{cluster_uid}-#{i+1}" do |node_uid|
-        supported_job_types({:deploy => true, :besteffort => true, :virtual => false})
-        architecture({
-          :smp_size => 2,
-          :smt_size => 2,
-          :platform_type => "x86_64"
-        })
-        processor({
-          :vendor => "AMD",
-          :model => "AMD Opteron",
-          :version => "246",
-          :clock_speed => 2.giga,
-          :instruction_set => "",
-          :other_description => "",
-          :cache_l1 => nil,
-          :cache_l1i => nil,
-          :cache_l1d => nil,
-          :cache_l2 => nil          
-        })
-        main_memory({
-          :ram_size => 2.GB(true), # bytes
-          :virtual_size => nil
-        })
-        operating_system({
-          :name => "Ubuntu",
-          :release => "6.10",
-          :version => nil,
-          :kernel => "2.6.19.1"
-        })
-        storage_devices [
-          {:interface => 'SATA', :size => 80.GB(false), :driver => "sata_nv"}
-          ]
-        network_adapters [
-          {:interface => 'InfiniBand 10G', :rate => 10.giga, 
-            :switch => "c6509-grid", :network_address => "#{node_uid}.#{site_uid}.grid5000.fr", :ip => dns_lookup("#{node_uid}.#{site_uid}.grid5000.fr"), 
-            :vendor => "InfiniHost", :version => "MT23108", :enabled => true},
-          {:interface => 'Ethernet', :rate => 1.giga, :enabled => true, :driver => "tg3"},
-          {:interface => 'Ethernet', :rate => 1.giga, :enabled => false, :driver => "tg3"}
-          ]        
-      end
-    end
-  end
-
+  
   cluster :paramount do |cluster_uid|
     model "Dell PowerEdge 1950"
     created_at Time.parse("2007-09-01").httpdate
