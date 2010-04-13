e09075e5ea0cfba961c6ecef4bdf0d8e12e49ef0
Guillaume Ranquet <guillaume.ranquet@inria.fr>
2010-04-13 17:43:20 +0200

    [Environments] Added lenny-x64-2.1 environements

--- generators/input/environments.rb
+++ generators/input/environments.rb
@@ -373,4 +373,49 @@ environment 'lenny-x64-big-2.0' do
   tcp_bandwidth 1.G
 end
 
+environment 'lenny-x64-base-2.1' do
+  state "stable"
+  file({:path => "/grid5000/images/lenny-x64-base-2.1.tgz", :md5 => "4d63ad5aa069916da98e3c074297db41"})
+  kernel "2.6.26.2"
+  valid_on "griffon , grelon , sagittaire , capricorne , netgdx , gdx , bordeplage , bordereau , parapide , paramount , paraquad , paradent , azur , helios , sol , suno , edel , genepi , adonis , chuque , chinqchint , chicon , chti"
+  based_on "Debian version lenny for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}, {:login => "g5k", :password => "grid5000"}]
+  applications "Vim, XEmacs, JED, nano, JOE, Perl, Python, Ruby".split(", ")
+  x11_forwarding true
+  max_open_files 8192
+  tcp_bandwidth 1.G
+end
+
+environment 'lenny-x64-nfs-2.1' do
+  state "stable"
+  file({:path => "/grid5000/images/lenny-x64-nfs-2.1.tgz", :md5 => "b0756d201e0719c40785b746b8d5f395"})
+  valid_on "griffon , grelon , sagittaire , capricorne , netgdx , gdx , bordeplage , bordereau , parapide , paramount , paraquad , paradent , azur , helios , sol , suno , edel , genepi , adonis , chuque , chinqchint , chicon , chti"
+  kernel "2.6.26.2"
+  based_on "Debian version lenny for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services ['ldap', 'nfs']
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications "Vim, XEmacs, JED, nano, JOE, Perl, Python, Ruby".split(", ")
+  x11_forwarding true
+  max_open_files 8192
+  tcp_bandwidth 1.G
+end
+
+environment 'lenny-x64-big-2.1' do
+  state "stable"
+  file({:path => "/grid5000/images/lenny-x64-big-2.1.tgz", :md5 => "b85027090ace01bb5fc34c951ec5d5b4"})
+  valid_on "griffon , grelon , sagittaire , capricorne , netgdx , gdx , bordeplage , bordereau , parapide , paramount , paraquad , paradent , azur , helios , sol , suno , edel , genepi , adonis , chuque , chinqchint , chicon , chti"
+  kernel "2.6.26.2"
+  based_on "Debian version lenny for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services ['ldap', 'nfs']
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications "Vim, XEmacs, JED, nano, JOE, Perl, Python, Ruby".split(", ")
+  x11_forwarding true
+  max_open_files 8192
+  tcp_bandwidth 1.G
+end
+
 
