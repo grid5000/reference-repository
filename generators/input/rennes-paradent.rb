1cdffa31615112ae1f775673b52acfb1ec018406
Pascal Morillon <pascal.morillon@irisa.fr>
2011-03-25 08:39:46 +0100

    [admin] Rennes - Split generator file, one per cluster

---
+++ generators/input/rennes-paradent.rb
@@ -0,0 +1,66 @@
+site :rennes do |site_uid|
+  
+  cluster :paradent do |cluster_uid|
+    model "Carry System"
+    created_at Time.parse("2009-02-01").httpdate
+    
+    64.times do |i|
+      node "#{cluster_uid}-#{i+1}" do |node_uid|
+        supported_job_types({:deploy => true, :besteffort => true, :virtual => "ivt"})
+        architecture({
+          :smp_size => 2, 
+          :smt_size => 8,
+          :platform_type => "x86_64"
+          })
+        processor({
+          :vendor => "Intel",
+          :model => "Intel Xeon",
+          :version => "L5420",
+          :clock_speed => 2.5.G,
+          :instruction_set => "",
+          :other_description => "",
+          :cache_l1 => nil,
+          :cache_l1i => nil,
+          :cache_l1d => nil,
+          :cache_l2 => nil
+        })
+        main_memory({
+          :ram_size => 32.GiB,
+          :virtual_size => nil
+        })
+        operating_system({
+          :name => "Debian",
+          :release => "5.0",
+          :version => nil,
+          :kernel => "2.6.26"
+        })
+        storage_devices [{
+          :interface => 'SATA',
+          :size => 160.GB,
+          :driver => "ata_piix"
+        }]
+        network_adapters [{
+          :interface => 'Ethernet',
+          :rate => 1.G,
+          :enabled => true,
+          :management => false,
+          :mountable => true,
+          :mounted => true,
+          :device => "eth0",
+          :driver => "e1000e",
+          :network_address => "#{node_uid}.#{site_uid}.grid5000.fr",
+          :ip => dns_lookup("#{node_uid}.#{site_uid}.grid5000.fr"),
+          :switch => "c6509-grid"
+        },
+        {
+          :interface => 'Ethernet',
+          :rate => 1.G,
+          :enabled => false,
+          :device => "eth1",
+          :driver => "e1000e"
+        }]        
+      end
+    end
+  end
+  
+end
---
+++ generators/input/rennes-parapide.rb
@@ -0,0 +1,81 @@
+site :rennes do |site_uid|
+  
+  cluster :parapide do |cluster_uid|
+    model "SUN FIRE X2270"
+    created_at Time.parse("2010-01-25").httpdate
+    
+    25.times do |i|
+      node "#{cluster_uid}-#{i+1}" do |node_uid|
+        supported_job_types({:deploy => true, :besteffort => true, :virtual => "ivt"})
+        architecture({
+          :smp_size => 2, 
+          :smt_size => 8,
+          :platform_type => "x86_64"
+          })
+        processor({
+          :vendor => "Intel",
+          :model => "Intel Xeon",
+          :version => "X5570",
+          :clock_speed => 2.93.G,
+          :instruction_set => "",
+          :other_description => "",
+          :cache_l1 => nil,
+          :cache_l1i => nil,
+          :cache_l1d => nil,
+          :cache_l2 => nil
+        })
+        main_memory({
+          :ram_size => 24.GiB,
+          :virtual_size => nil
+        })
+        operating_system({
+          :name => "Debian",
+          :release => "5.0",
+          :version => nil,
+          :kernel => "2.6.26"
+        })
+        storage_devices [{
+          :interface => 'SATA',
+          :size => 500.GB,
+          :driver => "ahci"
+        }]
+        network_adapters [{
+          :interface => 'Ethernet',
+          :rate => 1.G,
+          :enabled => true,
+          :mountable => true,
+          :mounted => true,
+          :device => "eth0",
+          :driver => "igb",
+          :network_address => "#{node_uid}.#{site_uid}.grid5000.fr",
+          :ip => dns_lookup("#{node_uid}.#{site_uid}.grid5000.fr"),
+          :switch => "c6509-grid"
+        },
+        {
+          :interface => 'Ethernet',
+          :rate => 1.G,
+          :enabled => false,
+          :mountable => false,
+          :mounted => false,
+          :device => "eth1",
+          :driver => "igb"
+        },
+        {
+          :interface => 'Infiniband',
+          :rate => 10.G,
+          :enabled => true,
+          :mountable => true,
+          :mounted => true,
+          :device => "ib0",
+          :driver => "mlx4_core",
+          :vendor => "Mellanox",
+          :version => "MT25418",
+          :network_address => "#{node_uid}-ib0.#{site_uid}.grid5000.fr",
+          :ip => dns_lookup("#{node_uid}-ib0.#{site_uid}.grid5000.fr")
+        }]
+      end
+    end
+    
+  end
+  
+end
