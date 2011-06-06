62572ec6e1d7e2e8c75c006286fae5684a250b56
Gaetan SIMO <gaetan.simo@inria.fr>
2011-06-06 20:35:48 +0200

    [orsay] updating network caracteristics.

--- generators/input/orsay-gdx.rb
+++ generators/input/orsay-gdx.rb
@@ -1,4 +1,15 @@
 require 'net/ssh'
+
+def switch_match_node(switch,id); switch != nil and (switch[:nodes][0] <= id and id <= switch[:nodes][1]); end
+def get_switch_for_node(switches,id)
+  switches.each{|k,v| 
+    arr = v.split(",").map{|s| s.to_i}
+    next if id < arr[0] or arr[1] < id
+    return lookup('orsay-network',k).merge({:nodes=>arr})
+  }
+  lookup('orsay-network',"switch-nil").merge({:nodes=>[0,0]})
+end
+
 site :orsay do |site_uid|
   ssh = Net::SSH.start("frontend.#{site_uid}.grid5000.fr","g5kadmin")
   
@@ -8,9 +19,13 @@ site :orsay do |site_uid|
     misc "bios:1.28/bcm:1.20.17/bmc:1.10/rsaII:1.00"
     
     # WARN: 2 nodes are missing (gdx-311 and gdx-312) and won't appear in the reference
-    1.upto(180+132-2) do |i|
+    1.upto(180+132-2) do |i| 
+      switch_eth0 = get_switch_for_node lookup("orsay-links","links-eth0","gdx"), i if not switch_match_node switch_eth0, i
+      switch_bmc = get_switch_for_node lookup("orsay-links","links-bmc","gdx"), i if not switch_match_node switch_bmc, i
+      switch_rsa = get_switch_for_node lookup("orsay-links","links-rsa","gdx"), i if not switch_match_node switch_rsa, i
+      switch_mx = get_switch_for_node lookup("orsay-links","links-mx","gdx"), i if not switch_match_node switch_mx, i
+      
       node "#{cluster_uid}-#{i}" do |node_uid|
-        rack_uid = node_uid.split("-")[1].to_i / 18
     
         supported_job_types({:deploy => true, :besteffort => true, :virtual => false})
         architecture({
@@ -46,7 +61,7 @@ site :orsay do |site_uid|
         network_adapters [{
             :interface => 'Ethernet',
             :device => 'bmc',
-            :rate => (eval lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'rate')),
+            :rate => (eval switch_bmc["rate"]),
             :mac => lookup('orsay-gdx', "#{node_uid}", 'mac_bmc'),
             :vendor => 'Broadcom',
             :version => 'NetXtreme BCM5780',
@@ -56,16 +71,19 @@ site :orsay do |site_uid|
             :mounted => false,
             :network_address => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
             :ip => dns_lookup_through_ssh(ssh,"#{node_uid}-bmc.#{site_uid}.grid5000.fr"),
-            :switch => (lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'name')+".#{site_uid}.grid5000.fr"),
-            :switch_ip => lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'ip'),
-            :switch_mac => lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'mac'),
-            :switch_console => (lookup('orsay-network', "switch-rsa-#{rack_uid/2}", 'name')+".#{site_uid}.grid5000.fr"),
-            :switch_console_ip => lookup('orsay-network', "switch-rsa-#{rack_uid/2}", 'ip'),
-            :switch_console_mac => lookup('orsay-network', "switch-rsa-#{rack_uid/2}", 'mac'),
+            :switch => (switch_bmc["name"]+".#{site_uid}.grid5000.fr"),
+            :switch_ip => switch_bmc["ip"],
+            :switch_mac => switch_bmc["mac"],
+#            :switch => (lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'name')+".#{site_uid}.grid5000.fr"),
+#            :switch_ip => lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'ip'),
+#            :switch_mac => lookup('orsay-network', "switch-bmc-#{rack_uid/2}", 'mac'),
+            :switch_console => (switch_rsa["name"]+".#{site_uid}.grid5000.fr"),
+            :switch_console_ip => switch_rsa["ip"],
+            :switch_console_mac => switch_rsa["mac"]
           },{
             :interface => 'Myrinet',
             :device => 'myri0',
-            :rate => (eval lookup('orsay-network', "switch-mx", 'rate')),
+            :rate => (eval switch_mx["rate"]),
             :mac => lookup('orsay-gdx', "#{node_uid}", 'mac_mx'),
             :vendor => 'Myrinet',
             :version => '10G-PCIE-8A-C',
@@ -76,13 +94,13 @@ site :orsay do |site_uid|
             :mounted => true,
             :network_address => "#{node_uid}-myri0.#{site_uid}.grid5000.fr",
             :ip => dns_lookup_through_ssh(ssh,"#{node_uid}-myri0.#{site_uid}.grid5000.fr"),
-            :switch => (lookup('orsay-network', "switch-mx", 'name')+".#{site_uid}.grid5000.fr"),
-            :switch_ip => lookup('orsay-network', "switch-mx", 'ip'),
-            :switch_mac => lookup('orsay-network', "switch-mx", 'mac')
+            :switch => (switch_mx["name"]+".#{site_uid}.grid5000.fr"),
+            :switch_ip => switch_mx["ip"],
+            :switch_mac => switch_mx["mac"]
           },{
             :interface => 'Ethernet',
             :device => 'eth0',
-            :rate => (eval lookup('orsay-network', "switch-eth0-#{rack_uid}", 'rate')),
+            :rate => (eval switch_eth0["rate"]),
             :mac => lookup('orsay-gdx', "#{node_uid}", 'mac_eth0'),
             :vendor => 'Broadcom',
             :version => 'NetXtreme BCM5780',
@@ -93,9 +111,9 @@ site :orsay do |site_uid|
             :mounted => true,
             :network_address => "#{node_uid}.#{site_uid}.grid5000.fr",
             :ip => dns_lookup_through_ssh(ssh,"#{node_uid}.#{site_uid}.grid5000.fr"),
-            :switch => (lookup('orsay-network', "switch-eth0-#{rack_uid}", 'name')+".#{site_uid}.grid5000.fr"),
-            :switch_ip => lookup('orsay-network', "switch-eth0-#{rack_uid}", 'ip'),
-            :switch_mac => lookup('orsay-network', "switch-eth0-#{rack_uid}", 'mac'),
+            :switch => (switch_eth0["name"]+".#{site_uid}.grid5000.fr"),
+            :switch_ip => switch_eth0["ip"],
+            :switch_mac => switch_eth0["mac"]
           },{
             :interface => 'Ethernet',
             :device => 'eth1',
