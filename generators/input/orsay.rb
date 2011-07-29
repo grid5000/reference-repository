4b75967178e750e525edc85126470a5a269a7856
Gaetan SIMO <gaetan.simo@inria.fr>
2011-07-29 17:22:41 +0200

    [orsay] updating network architecture.

--- generators/input/orsay.rb
+++ generators/input/orsay.rb
@@ -1,13 +1,3 @@
-require 'net/ssh'
-def switch_match_node(switch,id); switch != nil and (switch[:nodes][0] <= id and id <= switch[:nodes][1]); end
-def get_switch_for_node(switches,id)
-  switches.each{|k,v| 
-    arr = v.split(",").map{|s| s.to_i}
-    next if id < arr[0] or arr[1] < id
-    return lookup('orsay-network',k).merge({:nodes=>arr})
-  }
-  fail "Could not find your switch."
-end
 def node_switch(clusters,switch,node)
   cluster,uids,iface = node.split("-")
   clusters[cluster] = [] unless  clusters.has_key? cluster
@@ -54,25 +44,43 @@ def update_switch(switches,iface,sw_name)
     switches[iface] = sw
   end
 end
-def fill_iface(site_uid,node_uid,iface,switches)
-  {
-    :interface => (case iface;when "myri0";'Myrinet';else;"Ethernet";end),
-    :device => iface,
-    :rate => (eval switches[iface]["rate-in"]),
-    :mac => lookup('orsay-gdx', "#{node_uid}")[iface]["mac"],
-    :vendor => (case iface;when "myri0";'Myrinet';else;'Broadcom';end),
-    :version => (case iface;when "myri0";'10G-PCIE-8A-C';else;'NetXtreme BCM5780';end),
-    :enabled => (case iface;when "eth1";false;else;true;end),
-    :management => (case iface;when "bmc";true;else;false;end),
-    :mountable => (case iface;when "myri0","eth0";true;else;false;end),
-    :mounted => (case iface;when "myri0","eth0";true;else;false;end),
-    :driver => (case iface;when "myri0";'mx_driver';else;'tg3';end),
-    :network_address => (case iface;when "eth0";"#{node_uid}.#{site_uid}.grid5000.fr";else;"#{node_uid}-#{iface}.#{site_uid}.grid5000.fr";end),
-    :ip => lookup('orsay-gdx', "#{node_uid}")[iface]["ip"],
-    :switch =>  ((switches[iface]["name"] == "switch-nil") ? (nil) : (switches[iface]["name"]+".#{site_uid}.grid5000.fr")),
-    :switch_ip => switches[iface]["ip"],
-    :switch_mac => switches[iface]["mac"]
+def fill_net_ifaces(site_uid,cluster_uid,node_uid,ifaces,switches)
+  net_ifaces = []
+  ifaces.each{|iface,sw_name|
+    next if iface == "rsa"
+
+    node = lookup("#{site_uid}-#{cluster_uid}", "#{node_uid}")
+    fail "Node #{node_uid} not found in configuration '#{site_uid}-#{cluster_uid}'" if node.nil?
+  
+    net =   {
+      :interface => (case iface;when "myri0";'Myrinet';else;"Ethernet";end),
+      :device => iface,
+      :rate => (eval switches[iface]["rate-in"]),
+      :mac => node[iface]["mac"],
+      :vendor => (case iface;when "myri0";'Myrinet';else;'Broadcom';end),
+      :version => (case iface;when "myri0";'10G-PCIE-8A-C';else;'NetXtreme BCM5780';end),
+      :enabled => (case iface;when "eth1";false;else;true;end),
+      :management => (case iface;when "bmc";true;else;false;end),
+      :mountable => (case iface;when "myri0","eth0";true;else;false;end),
+      :mounted => (case iface;when "myri0","eth0";true;else;false;end),
+      :driver => (case iface;when "myri0";'mx_driver';else;'tg3';end),
+      :network_address => (case iface;when "eth0";"#{node_uid}.#{site_uid}.grid5000.fr";else;"#{node_uid}-#{iface}.#{site_uid}.grid5000.fr";end),
+      :ip => node[iface]["ip"],
+      :switch =>  ((switches[iface]["name"] == "switch-nil") ? (nil) : (switches[iface]["name"]+".#{site_uid}.grid5000.fr")),
+      :switch_ip => switches[iface]["ip"],
+      :switch_mac => switches[iface]["mac"]
+    } 
+    
+    if net[:device] == "bmc"
+      net.merge!({
+      :switch_console => (switches["rsa"]["name"]+".#{site_uid}.grid5000.fr"),
+      :switch_console_ip => switches["rsa"]["ip"],
+      :switch_console_mac => switches["rsa"]["mac"]
+      })
+    end
+    net_ifaces.push(net)
   }  
+  net_ifaces
 end
 
 site :orsay do |site_uid|
@@ -99,7 +107,7 @@ site :orsay do |site_uid|
     misc "bios:1.28/bcm:1.20.17/bmc:1.10/rsaII:1.00"
 
     switches = {}
-    clusters["gdx"].each.each_with_index {|ifaces, id|
+    clusters[cluster_uid].each.each_with_index {|ifaces, id|
       next if ifaces.nil?
       ifaces.each{|iface,sw_name| update_switch switches, iface, sw_name }
       
@@ -139,22 +147,59 @@ site :orsay do |site_uid|
           {:interface => 'SATA', :size => 80.GB, :driver => "sata_svw"}
           ]
 
-        net_ifaces = []
-        ifaces.each{|iface,sw_name|
-          next if iface == "rsa"
-          net = fill_iface site_uid, node_uid, iface, switches
-          if net[:device] == "bmc"
-            net.merge!({
-            :switch_console => (switches["rsa"]["name"]+".#{site_uid}.grid5000.fr"),
-            :switch_console_ip => switches["rsa"]["ip"],
-            :switch_console_mac => switches["rsa"]["mac"]
-            })
-          end
-          net_ifaces.push(net)
-        }
-#        puts net_ifaces.inspect
-        network_adapters net_ifaces
+        network_adapters (fill_net_ifaces(site_uid,cluster_uid,node_uid,ifaces,switches))
       end        
+    }
+  end
+  
+  cluster :netgdx do |cluster_uid|
+    model "IBM eServer 326m"
+    created_at nil
+    misc "bios:1.28/bcm:1.20.17/bmc:1.10/rsaII:1.00"
+
+    switches = {}
+    clusters[cluster_uid].each.each_with_index {|ifaces, id|
+      next if ifaces.nil?
+      ifaces.each{|iface,sw_name| update_switch switches, iface, sw_name }
+
+      node "#{cluster_uid}-#{id}" do |node_uid|
+        cmts = lookup('orsay-netgdx', "#{node_uid}", 'comments')
+        comments cmts unless cmts.nil?
+        
+        supported_job_types({:deploy => true, :besteffort => true, :virtual => false})
+        architecture({
+          :smp_size => 2, 
+          :smt_size => 2,
+          :platform_type => "x86_64"
+          })
+        processor({
+          :vendor => "AMD",
+          :model => "AMD Opteron",
+          :version => "246",
+          :clock_speed => 2.G,
+          :instruction_set => "",
+          :other_description => "",
+          :cache_l1 => nil,
+          :cache_l1i => nil,
+          :cache_l1d => nil,
+          :cache_l2 => 1.MiB
+        })
+        main_memory({
+          :ram_size => 2.GiB, # bytes
+          :virtual_size => nil
+        })
+        operating_system({
+          :name => "debian-x64-5-prod",
+          :release => "5.1.2",
+          :version => "5",
+          :kernel   => "2.6.26"
+        })
+        storage_devices [
+          {:interface => 'SATA', :size => 80.GB, :driver => "sata_svw"}
+          ]
+
+        network_adapters (fill_net_ifaces(site_uid,cluster_uid,node_uid,ifaces,switches))      
+      end
     }
   end
 end
