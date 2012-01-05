6c66360e8552dd788760d59b1d453a53a4ce4328
Sebastien Badia <sebastien.badia@inria.fr>
2012-01-05 21:54:00 +0100

    [envs] Add refs images 1.1

---
+++ generators/input/environments/squeeze-x64-nfs-1.1.rb
@@ -0,0 +1,15 @@
+environment 'squeeze-x64-nfs-1.1' do
+  state "stable"
+  file({:path => "/grid5000/images/squeeze-x64-nfs-1.1.tgz", :md5 => "5256bf32cb26ffd816dc9d7b1f510f6b"})
+  kernel "2.6.32-5"
+  available_on %w{bordeaux grenoble lille lyon luxembourg nancy orsay reims rennes sophia toulouse}
+  valid_on "bordeplage , bordereau , borderline , adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , capricorne , sagittaire , granduc , graphene , griffon , gdx , netgdx , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel , violette"
+  based_on "Debian version squeeze for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications %w{Vim XEmacs JED nano JOE Perl Python Ruby}
+  x11_forwarding true
+  max_open_files 65536
+  tcp_bandwidth 1.G
+end
