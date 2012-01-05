eb861a90ec23c28c3efbbd4257646fe75ed206d5
Sebastien Badia <sebastien.badia@inria.fr>
2012-01-05 15:12:31 +0100

    [luxembourg] Typo ivt support, and regenerate

--- generators/input/luxembourg-granduc.rb
+++ generators/input/luxembourg-granduc.rb
@@ -6,7 +6,7 @@ site :luxembourg do |site_uid|
 
     22.times do |i|
       node "#{cluster_uid}-#{i+1}" do |node_uid|
-        supported_job_types({:deploy => true, :besteffort => true, :virtual => ivt})
+        supported_job_types({:deploy => true, :besteffort => true, :virtual => "ivt"})
         architecture({
           :smp_size       => 2,
           :smt_size       => 8,
