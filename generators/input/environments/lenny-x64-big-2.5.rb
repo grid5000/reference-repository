e22a8385cc19a6d69aa8d9c57ebf7e1fb4b3e394
Tina Rakotoarivelo <tina.rakotoarivelo@inria.fr>
2012-04-10 14:17:41 +0200

    [environments] New reference images available on all sites

---
+++ generators/input/environments/lenny-x64-big-2.5.rb
@@ -0,0 +1,15 @@
+environment 'lenny-x64-big-2.5' do
+  state "stable"
+  file({:path => "/grid5000/images/lenny-x64-big-2.5.tgz", :md5 => "96651a7e269a711d76477b5f776ec6d1"})
+  kernel "2.6.26.2"
+  available_on %w{bordeaux grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "bordeplage , bordereau , borderline ,  adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc , capricorne , sagittaire , graphene , griffon , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel"
+  based_on "Debian version lenny for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications "Vim, XEmacs, JED, nano, JOE, Perl, Python, Ruby".split(", ")
+  x11_forwarding true
+  max_open_files 8192
+  tcp_bandwidth 1.G
+end
