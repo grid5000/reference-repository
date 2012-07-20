aaebd39a062405199444821eef4f5cb477ced533
Tina Rakotoarivelo <tina.rakotoarivelo@inria.fr>
2012-07-20 11:45:28 +0200

    [environments] New environments releases

---
+++ generators/input/environments/lenny-x64-base-2.6.rb
@@ -0,0 +1,15 @@
+environment 'lenny-x64-base-2.6' do
+  state "stable"
+  file({:path => "/grid5000/images/lenny-x64-base-2.6.tgz", :md5 => "be580611a1b93820afc12eda30455e41"})
+  kernel "2.6.26.2"
+  available_on %w{bordeaux grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "bordeplage , bordereau , borderline ,  adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc  , sagittaire , graphene , griffon , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel"
+  based_on "Debian version lenny for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications "Vim, XEmacs, JED, nano, JOE, Perl, Python, Ruby".split(", ")
+  x11_forwarding true
+  max_open_files 8192
+  tcp_bandwidth 1.G
+end
