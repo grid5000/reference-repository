d9157458ca237613332c950fc3263385b5d05da4
Vidal WATAT <vidal.watat@inria.fr>
2012-07-25 16:06:20 +0200

    [grenoble] Correct entries for kavlan end virt

--- generators/input/sites/grenoble/grenoble.rb
+++ generators/input/sites/grenoble/grenoble.rb
@@ -10,6 +10,6 @@ site :grenoble do |site_uid|
   security_contact "grenoble-staff@lists.grid5000.fr"
   user_support_contact "grenoble-staff@lists.grid5000.fr"
   compilation_server false
-  kavlan_plage "10.4.0.0/14"
-  virt_plage "10.132.0.0/14"
+  kavlan_ip_range "10.4.0.0/14"
+  virt_ip_range "10.132.0.0/14"
 end
