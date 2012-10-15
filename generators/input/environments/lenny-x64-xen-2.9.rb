0b8632db843c25f0e90ef817247d15bed33b8b89
Tina Rakotoarivelo <tina.rakotoarivelo@inria.fr>
2012-10-15 13:50:14 +0200

    [environments] Add lenny-x64-min-1.3, lenny-x64-base-2.7, lenny-x64-{nfs,big}-2.8, lenny-x64-xen-2.9, squeeze-x64-{min,base,xen}-1.4, squeeze-x64-{nfs,big}-1.5

---
+++ generators/input/environments/lenny-x64-xen-2.9.rb
@@ -0,0 +1,15 @@
+environment 'lenny-x64-xen-2.9' do
+  state "stable"
+  file({:path => "/grid5000/images/lenny-x64-xen-2.9.tgz", :md5 => "a4fdff535af0f59b9b1f5df8b59b2068"})
+  kernel "2.6.26.2"
+  available_on %w{bordeaux grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "bordeplage , bordereau , borderline ,  adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc , hercule, orion, sagittaire, taurus, graphene , griffon , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel"
+  based_on "Debian version lenny for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications %w{Vim XEmacs JED nano JOE Perl Python Ruby}
+  x11_forwarding true
+  max_open_files 8192
+  tcp_bandwidth 1.G
+end
