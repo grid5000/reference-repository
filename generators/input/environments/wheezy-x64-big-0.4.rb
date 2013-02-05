68167ac0b8fb4fb5af391369461d1fe4734066c2
Elodie Bertoncello <elodie.bertoncello@inria.fr>
2013-02-05 14:31:58 +0100

    [environments] add new reference images squeeze-x64-[min,base,xen]-1.6 squeeze-x64-[nfs,big]-1.7 and wheezy reference images

---
+++ generators/input/environments/wheezy-x64-big-0.4.rb
@@ -0,0 +1,15 @@
+environment 'wheezy-x64-big-0.4' do
+  state "stable"
+  file({:path => "/grid5000/images/wheezy-x64-big-0.4.tgz", :md5 => "563b958eaa74b7dbbec18d40b4ab53b6"})
+  kernel "3.2.0-4"
+  available_on %w{bordeaux grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "bordeplage , bordereau , borderline ,  adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc , hercule, orion, sagittaire, taurus, graphene , griffon , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel"
+  based_on "Debian version wheezy for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications %w{Vim XEmacs JED nano JOE Perl Python Ruby}
+  x11_forwarding true
+  max_open_files 65536
+  tcp_bandwidth 1.G
+end
---
+++ generators/input/environments/wheezy-x64-xen-0.4.rb
@@ -0,0 +1,15 @@
+environment 'wheezy-x64-xen-0.4' do
+  state "stable"
+  file({:path => "/grid5000/images/wheezy-x64-xen-0.4.tgz", :md5 => "a4889ddf8b509bb17e799684aa7367ca"})
+  kernel "3.2.0-4"
+  available_on %w{bordeaux grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "bordereau , borderline ,  adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc , hercule, orion, sagittaire, taurus, graphene , griffon , stremi , paradent , paramount , parapide , parapluie , sol , suno, pastel"
+  based_on "Debian version wheezy for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications %w{Vim XEmacs JED nano JOE Perl Python Ruby}
+  x11_forwarding true
+  max_open_files 65536
+  tcp_bandwidth 1.G
+end
