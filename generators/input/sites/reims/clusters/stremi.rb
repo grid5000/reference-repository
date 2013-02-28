d37f65c3a8080718488e6e3eeaa233e6a6037617
Vidal WATAT <vidal.watat@inria.fr>
2013-02-28 11:47:15 +0100

    [reims] - Performance informations -

--- generators/input/sites/reims/clusters/stremi.rb
+++ generators/input/sites/reims/clusters/stremi.rb
@@ -8,8 +8,8 @@ site :reims do |site_uid|
        node "#{cluster_uid}-#{i+1}" do |node_uid|
          supported_job_types({:deploy => true, :besteffort => true, :virtual => "amd-v"})
          performance({
-           :node_flops => 4931000000,
-           :core_flops => 121300000000
+           :core_flops => 4931000000,
+           :node_flops => 121300000000
          })
          architecture({
            :smp_size       => 2,
