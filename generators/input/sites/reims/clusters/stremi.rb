fc346a9ef365ead4c69b26fe40c88d2ff4ba2dde
Émile Morel <emile.morel@inria.fr>
2013-05-03 14:15:29 +0200

    [Reims] stremi - values from g5kchecks

--- generators/input/sites/reims/clusters/stremi.rb
+++ generators/input/sites/reims/clusters/stremi.rb
@@ -1,119 +1,166 @@
 site :reims do |site_uid|
-  cluster :stremi do |cluster_uid|
-     model "HP ProLiant DL165 G7"
-     created_at Time.parse("2011-04-18").httpdate
-     kavlan true
 
-     44.times do |i|
-       node "#{cluster_uid}-#{i+1}" do |node_uid|
-         supported_job_types({:deploy => true, :besteffort => true, :virtual => "amd-v"})
-         performance({
-           :core_flops => 4931000000,
-           :node_flops => 121300000000
-         })
-         architecture({
-           :smp_size       => 2,
-           :smt_size       => 24,
-           :platform_type  => "amd64"
-           })
-         processor({
-           :vendor             => "AMD",
-           :model              => "AMD Opteron",
-           :version            => "6164 HE",
-           :clock_speed        => 1.7.G,
-           :instruction_set    => "",
-           :other_description  => "",
-           :cache_l1           => nil,
-           :cache_l1i          => nil,
-           :cache_l1d          => nil,
-           :cache_l2           => nil
-         })
-         main_memory({
-           :ram_size     => 48.GiB,
-           :virtual_size => nil
-         })
-         operating_system({
-           :name     => "Debian",
-           :release  => "6.0",
-           :version  => nil,
-           :kernel   => "2.6.32"
-         })
-         storage_devices [{
-           :interface => 'SATA',
-           :size => 250.GB,
-           :driver => "ahci",
-           :device => "sda"
-         }]
-         network_adapters [{
-           :interface        => 'Ethernet',
-           :rate             => 1.G,
-           :enabled          => true,
-           :management       => true,
-           :mountable        => false,
-           :mounted          => false,
-           :device           => "bmc",
-           :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
-           :ip               => lookup('stremi', node_uid, 'network_interfaces', 'bmc', 'ip'),
-           :mac              => lookup('stremi', node_uid, 'network_interfaces', 'bmc', 'mac')
-         },
-         {
-           :interface        => 'Ethernet',
-           :rate             => 1.G,
-           :enabled          => true,
-           :mountable        => true,
-           :mounted          => true,
-           :bridged 	     => true,
-           :device           => "eth0",
-           :driver           => "igb",
-           :network_address  => "#{node_uid}.#{site_uid}.grid5000.fr",
-           :ip               => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'ip'),
-           :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'mac'),
-           :switch           => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'switch_name'),
-           :switch_port      => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'switch_port')
-         },
-         {
-           :interface        => 'Ethernet',
-           :rate             => 1.G,
-           :enabled          => false,
-           :device           => "eth1",
-           :driver           => "igb",
-           :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'mac')
-         },
-         {
-           :interface        => 'Ethernet',
-           :rate             => 1.G,
-           :enabled          => false,
-           :device           => "eth2",
-           :driver           => "igb",
-           :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'mac')
-         },
-         {
-           :interface        => 'Ethernet',
-           :rate             => 1.G,
-           :enabled          => false,
-           :device           => "eth3",
-           :driver           => "igb",
-           :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'mac')
-         }]
-         gpu({
-           :gpu  => false
-            })
-         monitoring({
-           :wattmeter  => true
-         })
-         sensors({
-           :power => {
-           :available => true,
-           :via => {
-             :api => { :metric => 'pdu' },
-             :pdu => {
-               :uid  => lookup('stremi', node_uid, 'pdu', 'pdu_name'),
-               :port => lookup('stremi', node_uid, 'pdu', 'pdu_position'),
-               }
-             }
-           }
-         })
-       end
-     end
+  cluster :stremi do |cluster_uid|
+    model "HP ProLiant DL165 G7"
+    created_at Time.parse("2011-04-18").httpdate
+    kavlan true
+
+    44.times do |i|
+      node "#{cluster_uid}-#{i+1}" do |node_uid|
+
+        performance({
+        :core_flops => 4931000000,
+        :node_flops => 121300000000
+      })
+
+        supported_job_types({
+          :deploy       => true,
+          :besteffort   => true,
+          :virtual      => lookup('stremi', node_uid, 'supported_job_types', 'virtual')
+        })
+
+        architecture({
+          :smp_size       => lookup('stremi', node_uid, 'architecture', 'smp_size'),
+          :smt_size       => lookup('stremi', node_uid, 'architecture', 'smt_size'),
+          :platform_type  => lookup('stremi', node_uid, 'architecture', 'platform_type')
+        })
+
+        processor({
+          :vendor             => lookup('stremi', node_uid, 'processor', 'vendor'),
+          :model              => lookup('stremi', node_uid, 'processor', 'model'),
+          :version            => lookup('stremi', node_uid, 'processor', 'version'),
+          :clock_speed        => lookup('stremi', node_uid, 'processor', 'clock_speed'),
+          :instruction_set    => lookup('stremi', node_uid, 'processor', 'instruction_set'),
+          :other_description  => lookup('stremi', node_uid, 'processor', 'other_description'),
+          :cache_l1           => lookup('stremi', node_uid, 'processor', 'cache_l1'),
+          :cache_l1i          => lookup('stremi', node_uid, 'processor', 'cache_l1i'),
+          :cache_l1d          => lookup('stremi', node_uid, 'processor', 'cache_l1d'),
+          :cache_l2           => lookup('stremi', node_uid, 'processor', 'cache_l2'),
+          :cache_l3           => lookup('stremi', node_uid, 'processor', 'cache_l3')
+        })
+
+        main_memory({
+          :ram_size     => lookup('stremi', node_uid, 'main_memory', 'ram_size'),
+          :virtual_size => nil
+        })
+
+        operating_system({
+          :name     => lookup('stremi', node_uid, 'operating_system', 'name'),
+          :release  => "Squeeze",
+          :version  => lookup('stremi', node_uid, 'operating_system', 'version'),
+          :kernel   => lookup('stremi', node_uid, 'operating_system', 'kernel')
+        })
+
+        storage_devices [{
+          :interface  => 'SATA',
+          :size       => lookup('stremi', node_uid, 'block_devices', 'sda', 'size'),
+          :driver     => "ahci",
+          :device     => lookup('stremi', node_uid, 'block_devices', 'sda', 'device'),
+          :model      => lookup('stremi', node_uid, 'block_devices', 'sda', 'model'),
+          :vendor     => lookup('stremi', node_uid, 'block_devices', 'sda', 'vendor'),
+          :rev        => lookup('stremi', node_uid, 'block_devices', 'sda', 'rev')
+        }]
+
+        network_adapters [{
+          :interface        => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'interface'),
+          :rate             => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'rate'),
+          :enabled          => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'enabled'),
+          :management       => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'management'),
+          :mountable        => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'mountable'),
+          :mounted          => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'mounted'),
+          :bridged          => true,
+          :device           => "eth0",
+          :driver           => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'driver'),
+          :network_address  => "#{node_uid}.#{site_uid}.grid5000.fr",
+          :ip               => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'ip'),
+          :ip6              => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'ip6'),
+          :switch           => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'switch_name'),
+          :switch_port      => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'switch_port'),
+          :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth0', 'mac')
+        },
+        {
+          :interface        => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'interface'),
+          :rate             => 1.G,
+          :enabled          => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'enabled'),
+          :management       => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'management'),
+          :mountable        => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'mountable'),
+          :mounted          => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'mounted'),
+          :bridged          => false,
+          :device           => "eth1",
+          :driver           => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'driver'),
+          :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth1', 'mac')
+        },
+        {
+          :interface        => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'interface'),
+          :rate             => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'rate'),
+          :enabled          => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'enabled'),
+          :management       => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'management'),
+          :mountable        => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'mountable'),
+          :mounted          => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'mounted'),
+          :bridged          => false,
+          :device           => "eth2",
+          :driver           => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'driver'),
+          :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth2', 'mac')
+        },
+        {
+          :interface        => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'interface'),
+          :rate             => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'rate'),
+          :enabled          => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'enabled'),
+          :management       => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'management'),
+          :mountable        => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'mountable'),
+          :mounted          => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'mounted'),
+          :bridged          => false,
+          :device           => "eth3",
+          :driver           => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'driver'),
+          :mac              => lookup('stremi', node_uid, 'network_interfaces', 'eth3', 'mac')
+        },
+        {
+          :interface        => 'Ethernet',
+          :rate             => 1.G,
+          :enabled          => true,
+          :management       => true,
+          :mountable        => false,
+          :mounted          => false,
+          :device           => "bmc",
+          :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
+          :ip               => lookup('stremi', node_uid, 'network_interfaces', 'bmc', 'ip'),
+          :mac              => lookup('stremi', node_uid, 'network_interfaces', 'bmc', 'mac'),
+        }]
+
+        chassis({
+          :serial       => lookup('stremi', node_uid, 'chassis', 'serial_number'),
+          :name         => lookup('stremi', node_uid, 'chassis', 'product_name'),
+          :manufacturer => lookup('stremi', node_uid, 'chassis', 'manufacturer')
+        })
+
+        bios({
+          :version      => lookup('stremi', node_uid, 'bios', 'version'),
+          :vendor       => lookup('stremi', node_uid, 'bios', 'vendor'),
+          :release_date => lookup('stremi', node_uid, 'bios', 'release_date')
+        })
+
+        gpu({
+          :gpu  => false
+        })
+
+        monitoring({
+          :wattmeter  => true
+        })
+
+        sensors({
+          :power => {
+            :available => true,
+            :via => {
+              :api => { :metric => 'pdu' },
+              :pdu => {
+                :uid  => lookup('stremi', node_uid, 'pdu', 'pdu_name'),
+                :port => lookup('stremi', node_uid, 'pdu', 'pdu_position'),
+              }
+            }
+          }
+        })
+
+      end
+    end
   end
 end
