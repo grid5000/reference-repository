295f57fe7ca30896733828b70fb727bba1fc06e3
Émile Morel <emile.morel@inria.fr>
2013-05-11 10:34:56 +0200

    [rennes] paradent - values from g5kchecks

--- generators/input/sites/rennes/clusters/paradent.rb
+++ generators/input/sites/rennes/clusters/paradent.rb
@@ -8,11 +8,11 @@ site :rennes do |site_uid|
     64.times do |i|
       node "#{cluster_uid}-#{i+1}" do |node_uid|
 
-        supported_job_types({:deploy => true, :besteffort => true, :virtual => "ivt"})
-         performance({
-           :core_flops => 8095000000,
-           :node_flops => 55530000000
-         })
+        performance({
+        :core_flops => 8095000000,
+        :node_flops => 55530000000
+      })
+
         architecture({
           :smp_size       => 2,
           :smt_size       => 8,
@@ -32,81 +32,105 @@ site :rennes do |site_uid|
           :cache_l2           => nil
         })
 
+        supported_job_types({
+          :deploy       => true,
+          :besteffort   => true,
+          :virtual      => lookup('paradent', node_uid, 'supported_job_types', 'virtual')
+        })
+
+        architecture({
+          :smp_size       => lookup('paradent', node_uid, 'architecture', 'smp_size'),
+          :smt_size       => lookup('paradent', node_uid, 'architecture', 'smt_size'),
+          :platform_type  => lookup('paradent', node_uid, 'architecture', 'platform_type')
+        })
+
+        processor({
+          :vendor             => lookup('paradent', node_uid, 'processor', 'vendor'),
+          :model              => lookup('paradent', node_uid, 'processor', 'model'),
+          :version            => lookup('paradent', node_uid, 'processor', 'version'),
+          :clock_speed        => lookup('paradent', node_uid, 'processor', 'clock_speed'),
+          :instruction_set    => lookup('paradent', node_uid, 'processor', 'instruction_set'),
+          :other_description  => lookup('paradent', node_uid, 'processor', 'other_description'),
+          :cache_l1           => lookup('paradent', node_uid, 'processor', 'cache_l1'),
+          :cache_l1i          => lookup('paradent', node_uid, 'processor', 'cache_l1i'),
+          :cache_l1d          => lookup('paradent', node_uid, 'processor', 'cache_l1d'),
+          :cache_l2           => lookup('paradent', node_uid, 'processor', 'cache_l2'),
+          :cache_l3           => lookup('paradent', node_uid, 'processor', 'cache_l3')
+        })
+
         main_memory({
-          :ram_size     => 32.GiB,
+          :ram_size     => lookup('paradent', node_uid, 'main_memory', 'ram_size'),
           :virtual_size => nil
         })
 
         operating_system({
-          :name     => "Debian",
-          :release  => "5.0",
-          :version  => nil,
-          :kernel   => "2.6.26"
+          :name     => lookup('paradent', node_uid, 'operating_system', 'name'),
+          :release  => "Squeeze",
+          :version  => lookup('paradent', node_uid, 'operating_system', 'version'),
+          :kernel   => lookup('paradent', node_uid, 'operating_system', 'kernel')
         })
 
         storage_devices [{
           :interface  => 'SATA',
-          :size       => 160.GB,
+          :size       => lookup('paradent', node_uid, 'block_devices', 'sda', 'size'),
           :driver     => "ata_piix",
-          :device     => "sda",
-          :model      => lookup('paradent', node_uid, 'block_device', 'sda', 'model'),
-          :rev        => lookup('paradent', node_uid, 'block_device', 'sda', 'rev')
+          :device     => lookup('paradent', node_uid, 'block_devices', 'sda', 'device'),
+          :model      => lookup('paradent', node_uid, 'block_devices', 'sda', 'model'),
+          :vendor     => lookup('paradent', node_uid, 'block_devices', 'sda', 'vendor'),
+          :rev        => lookup('paradent', node_uid, 'block_devices', 'sda', 'rev')
         }]
 
         network_adapters [{
-          :interface        => 'Ethernet',
-          :rate             => 1.G,
-          :enabled          => true,
-          :management       => true,
-          :mountable        => false,
-          :mounted          => false,
-          :device           => "bmc",
-          :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
-          :ip               => lookup('paradent', node_uid, 'network_interfaces', 'bmc', 'ip'),
-          :mac              => lookup('paradent', node_uid, 'network_interfaces', 'bmc', 'mac')
-        },
-        {
-          :interface        => 'Ethernet',
-          :rate             => 1.G,
-          :enabled          => true,
-          :bridged 	    => true,
-          :management       => false,
-          :mountable        => true,
-          :mounted          => true,
-          :device           => "eth0",
-          :driver           => "e1000e",
-          :network_address  => "#{node_uid}.#{site_uid}.grid5000.fr",
-          :ip               => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'ip'),
-          :switch           => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'switch_name'),
-          :switch_port      => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'switch_port'),
-          :mac              => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'mac'),
-          :vendor           => "Intel",
-          :version          => "80003ES2LAN"
-        },
-        {
-          :interface  => 'Ethernet',
-          :rate       => 1.G,
-          :enabled    => false,
-          :device     => "eth1",
-          :driver     => "e1000e",
+          :interface        => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'interface'),
+          :rate             => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'rate'),
+          :enabled          => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'enabled'),
+          :management       => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'management'),
+          :mountable        => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'mountable'),
+          :mounted          => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'mounted'),
+          :bridged          => true,
           :vendor           => "Intel",
           :version          => "80003ES2LAN",
-          :mac        => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'mac')
+          :device           => "eth0",
+          :driver           => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'driver'),
+          :network_address  => "#{node_uid}.#{site_uid}.grid5000.fr",
+          :ip               => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'ip'),
+          :ip6              => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'ip6'),
+          :switch           => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'switch_name'),
+          :switch_port      => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'switch_port'),
+          :mac              => lookup('paradent', node_uid, 'network_interfaces', 'eth0', 'mac')
         },
         {
-          :interface        => 'Ethernet',
-          :rate             => 100.M,
-          :enabled          => true,
-          :management       => true,
-          :mountable        => false,
-          :mounted          => false,
-          :device           => "bmc",
-          :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
-          :ip               => lookup('paradent', node_uid, 'network_interfaces', 'bmc', 'ip'),
-          :mac              => lookup('paradent', node_uid, 'network_interfaces', 'bmc', 'mac'),
-          :vendor           => "Tyan",
-          :version          => "M3296"
+          :interface        => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'interface'),
+          :rate             => 1.G,
+          :enabled          => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'enabled'),
+          :management       => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'management'),
+          :mountable        => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'mountable'),
+          :mounted          => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'mounted'),
+          :bridged          => false,
+          :device           => "eth1",
+          :vendor           => "Intel",
+          :version          => "80003ES2LAN",
+          :driver           => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'driver'),
+          :mac              => lookup('paradent', node_uid, 'network_interfaces', 'eth1', 'mac')
+        },
+        { :interface            => 'Ethernet',
+          :rate                 => 100.M,
+          :mac                  => lookup('paradent', node_uid, 'network_interfaces','bmc','mac'),
+          :vendor               => "Tyan",
+          :version              => "M3296",
+          :enabled              => true,
+          :management           => true,
+          :mountable            => false,
+          :mounted              => false,
+          :network_address      => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
+          :ip                   => lookup('paradent', node_uid,'network_interfaces','bmc','ip'),
         }]
+
+        chassis({
+          :serial       => lookup('paradent', node_uid, 'chassis', 'serial_number'),
+          :name         => lookup('paradent', node_uid, 'chassis', 'product_name'),
+          :manufacturer => lookup('paradent', node_uid, 'chassis', 'manufacturer')
+        })
 
         bios({
           :version      => lookup('paradent', node_uid, 'bios', 'version'),
