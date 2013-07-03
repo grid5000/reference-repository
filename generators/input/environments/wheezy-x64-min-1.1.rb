17b570d63a729c0457638ddd04ed4780b3a012d1
Elodie Bertoncello <elodie.bertoncello@inria.fr>
2013-07-03 17:26:06 +0200

    [environments] Release of wheezy 1.1 reference envs

---
+++ generators/input/environments/wheezy-x64-min-1.1.rb
@@ -0,0 +1,14 @@
+environment 'wheezy-x64-min-1.1' do
+  state "stable"
+  file({:path => "/grid5000/images/wheezy-x64-min-1.1.tgz", :md5 => "34d3b23067fb5c23a5b07b5c27a1a2ed"})
+  kernel "3.2.0-4"
+  available_on %w{bordeaux grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "bordeplage , bordereau , borderline ,  adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc , hercule, orion, sagittaire, taurus, graphene , griffon , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel"
+  based_on "Debian version wheezy for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications %w{Vim nano Perl}
+  x11_forwarding true
+  tcp_bandwidth 1.G
+end
