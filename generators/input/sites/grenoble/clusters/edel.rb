a39ee439c60f70235828347cc008b20844da2c90
Émile Morel <emile.morel@inria.fr>
2013-08-20 14:11:21 +0200

    [grenoble] edel - add information from g5kchecks

--- generators/input/sites/grenoble/clusters/edel.rb
+++ generators/input/sites/grenoble/clusters/edel.rb
@@ -3,131 +3,156 @@ site :grenoble do |site_uid|
   cluster :edel do |cluster_uid|
     model "Bull bullx B500 compute blades"
     created_at Time.parse("2008-10-03").httpdate
-    kavlan true
+    kavlan false
 
     72.times do |i|
       node "#{cluster_uid}-#{i+1}" do |node_uid|
-        supported_job_types({:deploy => true, :besteffort => true, :virtual => "ivt"})
+
         performance({
-          :core_flops => 71394000000,
-          :node_flops => 55450000000
+        :core_flops => 71394000000,
+        :node_flops => 55450000000
+      })
+
+        supported_job_types({
+          :deploy       => true,
+          :besteffort   => true,
+          :virtual      => lookup('edel', node_uid, 'supported_job_types', 'virtual')
         })
+
         architecture({
-          :smp_size => 2,
-          :smt_size => 8,
-          :platform_type => "x86_64"
+          :smp_size       => lookup('edel', node_uid, 'architecture', 'smp_size'),
+          :smt_size       => lookup('edel', node_uid, 'architecture', 'smt_size'),
+          :platform_type  => lookup('edel', node_uid, 'architecture', 'platform_type')
         })
+
         processor({
-          :vendor => "Intel",
-          :model => "Intel Xeon",
-          :version => "E5520",
-          :clock_speed => 2.27.G,
-          :instruction_set => "",
-          :other_description => "",
-          :cache_l1 => nil,
-          :cache_l1i => nil,
-          :cache_l1d => nil,
-          :cache_l2 => nil
+          :vendor             => lookup('edel', node_uid, 'processor', 'vendor'),
+          :model              => lookup('edel', node_uid, 'processor', 'model'),
+          :version            => lookup('edel', node_uid, 'processor', 'version'),
+          :clock_speed        => lookup('edel', node_uid, 'processor', 'clock_speed'),
+          :instruction_set    => lookup('edel', node_uid, 'processor', 'instruction_set'),
+          :other_description  => lookup('edel', node_uid, 'processor', 'other_description'),
+          :cache_l1           => lookup('edel', node_uid, 'processor', 'cache_l1'),
+          :cache_l1i          => lookup('edel', node_uid, 'processor', 'cache_l1i'),
+          :cache_l1d          => lookup('edel', node_uid, 'processor', 'cache_l1d'),
+          :cache_l2           => lookup('edel', node_uid, 'processor', 'cache_l2'),
+          :cache_l3           => lookup('edel', node_uid, 'processor', 'cache_l3')
         })
+
         main_memory({
-          :ram_size => 24.GiB, # bytes
+          :ram_size     => lookup('edel', node_uid, 'main_memory', 'ram_size'),
           :virtual_size => nil
         })
+
         operating_system({
-          :name => "Debian",
-          :release => "Squeeze",
-          :version => "6.0",
-          :kernel => "2.6.32"
+          :name     => lookup('edel', node_uid, 'operating_system', 'name'),
+          :release  => "Squeeze",
+          :version  => lookup('edel', node_uid, 'operating_system', 'version'),
+          :kernel   => lookup('edel', node_uid, 'operating_system', 'kernel')
         })
+
         storage_devices [{
-          :interface => 'SATA',
-          :size => 60.GB,
-          :driver => "ahci",
-          :model => lookup('edel', node_uid, 'block_devices', 'sda', 'model')
+          :interface  => 'SATA',
+          :size       => lookup('edel', node_uid, 'block_devices', 'sda', 'size'),
+          :driver     => "ahci",
+          :device     => lookup('edel', node_uid, 'block_devices', 'sda', 'device'),
+          :model      => lookup('edel', node_uid, 'block_devices', 'sda', 'model'),
+          :vendor     => lookup('edel', node_uid, 'block_devices', 'sda', 'vendor'),
+          :rev        => lookup('edel', node_uid, 'block_devices', 'sda', 'rev')
         }]
+
         network_adapters [{
-          :interface => 'InfiniBand',
-          :rate => 40.G,
-          :enabled => true,
-          :device => "ib0",
-          :mounted => true,
-          :mountable => true,
-          :management => false,
-          :vendor => 'Mellanox',
-          :version => "MT26428 ConnectX IB QDR, PCIe 2.0 5.0GT/s (rev a0)",
-          :driver => "mlx4_core",
-          :network_address => "#{node_uid}-ib0.#{site_uid}.grid5000.fr",
-          :ip => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'ip'),
-          :guid => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'guid')
+          :interface        => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'interface'),
+          :rate             => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'rate'),
+          :enabled          => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'enabled'),
+          :management       => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'management'),
+          :mountable        => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'mountable'),
+          :mounted          => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'mounted'),
+          :bridged          => true,
+          :device           => "eth0",
+          :vendor           => 'Intel',
+          :version          => "82576 Gigabit Network Connection",
+          :driver           => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'driver'),
+          :network_address  => "#{node_uid}.#{site_uid}.grid5000.fr",
+          :ip               => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'ip'),
+          :ip6              => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'ip6'),
+          :switch           => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'switch_name'),
+          :switch_port      => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'switch_port'),
+          :mac              => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'mac')
         },
         {
-          :interface => 'InfiniBand',
-          :rate => 40.G,
-          :enabled => false,
-          :device => "ib1",
-          :mounted => false,
-          :mountable => false,
-          :management => false,
-          :vendor => 'Mellanox',
-          :version => "MT26428 ConnectX IB QDR, PCIe 2.0 5.0GT/s (rev a0)",
-          :driver => "mlx4_core",
-          :guid => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'guid')
+          :interface        => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'interface'),
+          :rate             => 1.G,
+          :enabled          => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'enabled'),
+          :management       => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'management'),
+          :mountable        => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'mountable'),
+          :mounted          => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'mounted'),
+          :bridged          => false,
+          :device           => "eth1",
+          :vendor           => 'Intel',
+          :version          => "82576 Gigabit Network Connection",
+          :driver           => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'driver'),
+          :mac              => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'mac')
         },
         {
-          :interface => 'Ethernet',
-          :rate => 1.G,
-          :enabled => true,
-          :device => "eth0",
-          :mounted => true,
-          :mountable => true,
-          :bridged => true,
-          :management => false,
-          :network_address => "#{node_uid}.#{site_uid}.grid5000.fr",
-          :ip => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'ip'),
-          :mac => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'mac'),
-          :vendor => "Intel",
-          :version => "82576 Gigabit Network Connection",
-          :driver => "igb",
-          :switch => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'switch_name'),
-          :switch_port => lookup('edel', node_uid, 'network_interfaces', 'eth0', 'switch_port')
+          :interface        => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'interface'),
+          :rate             => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'rate'),
+          :device           => "ib0",
+          :enabled          => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'enabled'),
+          :management       => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'management'),
+          :mountable        => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'mountable'),
+          :mounted          => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'mounted'),
+          :vendor           => 'Mellanox',
+          :version          => "MT26428 ConnectX IB QDR, PCIe 2.0 5.0GT/s (rev a0)",
+          :driver           => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'driver'),
+          :network_address  => "#{node_uid}-ib0.#{site_uid}.grid5000.fr",
+          :ip               => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'ip'),
+          :guid             => lookup('edel', node_uid, 'network_interfaces', 'ib0', 'guid')
         },
         {
-          :interface => 'Ethernet',
-          :rate => 1.G,
-          :device => "eth1",
-          :enabled => false,
-          :mounted => false,
-          :mountable => false,
-          :management => false,
-          :vendor => "Intel",
-          :version => "82576 Gigabit Network Connection",
-          :mac => lookup('edel', node_uid, 'network_interfaces', 'eth1', 'mac')
+          :interface        => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'interface'),
+          :rate             => 40.G,
+          :rate             => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'rate'),
+          :device           => "ib1",
+          :enabled          => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'enabled'),
+          :management       => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'management'),
+          :mountable        => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'mountable'),
+          :mounted          => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'mounted'),
+          :vendor           => 'Mellanox',
+          :version          => "MT26428 ConnectX IB QDR, PCIe 2.0 5.0GT/s (rev a0)",
+          :driver           => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'driver'),
+          :guid             => lookup('edel', node_uid, 'network_interfaces', 'ib1', 'guid')
         },
         {
-          :interface => 'Ethernet',
-          :rate => 100.M,
-          :enabled => true,
-          :mounted => false,
-          :mountable => false,
-          :management => true,
-          :network_address => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
-          :ip => lookup('edel', node_uid, 'network_interfaces', 'bmc', 'ip'),
-          :mac => lookup('edel', node_uid, 'network_interfaces', 'bmc', 'mac'),
-          :vendor => "Unknown",
-          :version => "1.7"
+          :interface        => 'Ethernet',
+          :rate             => 100.M,
+          :enabled          => true,
+          :management       => true,
+          :mountable        => false,
+          :mounted          => false,
+          :device           => "bmc",
+          :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
+          :ip               => lookup('edel', node_uid, 'network_interfaces', 'bmc', 'ip'),
+          :vendor           => "Unknown",
+          :version          => "1.7",
+          :mac              => lookup('edel', node_uid, 'network_interfaces', 'bmc', 'mac')
         }]
+
+        chassis({
+          :serial       => lookup('edel', node_uid, 'chassis', 'serial_number'),
+          :name         => lookup('edel', node_uid, 'chassis', 'product_name'),
+          :manufacturer => lookup('edel', node_uid, 'chassis', 'manufacturer')
+        })
+
         bios({
           :version      => lookup('edel', node_uid, 'bios', 'version'),
           :vendor       => lookup('edel', node_uid, 'bios', 'vendor'),
           :release_date => lookup('edel', node_uid, 'bios', 'release_date')
         })
-        chassis({
-          :serial       => lookup('edel', node_uid, 'chassis', 'serial_number'),
-          :name         => lookup('edel', node_uid, 'chassis', 'product_name')
+
+        gpu({
+          :gpu  => false
         })
-         gpu({
-           :gpu  => false
-             })
 
         monitoring({
           :wattmeter  => true
@@ -142,8 +167,8 @@ site :grenoble do |site_uid|
             }
           }
         })
+
       end
     end
   end
-
 end
