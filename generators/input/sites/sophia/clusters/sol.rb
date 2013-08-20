df3bc1f9c942f3766495cac5a150a30720a7c4e4
Émile Morel <emile.morel@inria.fr>
2013-08-20 16:38:24 +0200

    [sophia] - values from g5k-checks

--- generators/input/sites/sophia/clusters/sol.rb
+++ generators/input/sites/sophia/clusters/sol.rb
@@ -4,138 +4,147 @@ site :sophia do |site_uid|
     model "Sun Fire X2200 M2"
     created_at Time.parse("2007-02-23").httpdate
     kavlan true
+
     50.times do |i|
       node "#{cluster_uid}-#{i+1}" do |node_uid|
-        supported_job_types({:deploy => true, :besteffort => true, :virtual => "amd-v"})
+
         performance({
           :core_flops => 4337000000,
           :node_flops => 16800000000
         })
 
+        supported_job_types({
+          :deploy       => true,
+          :besteffort   => true,
+          :virtual      => lookup('sol', node_uid, 'supported_job_types', 'virtual')
+        })
+
         architecture({
-          :smp_size => 2,
-          :smt_size => 4,
-          :platform_type => "x86_64"
+          :smp_size       => lookup('sol', node_uid, 'architecture', 'smp_size'),
+          :smt_size       => lookup('sol', node_uid, 'architecture', 'smt_size'),
+          :platform_type  => lookup('sol', node_uid, 'architecture', 'platform_type')
         })
+
         processor({
-          :vendor => "AMD",
-          :model => "AMD Opteron",
-          :version => "2218",
-          :clock_speed => 2.6.G,
-          :instruction_set => "",
-          :other_description => "",
-          :cache_l1 => nil,
-          :cache_l1i => nil,
-          :cache_l1d => nil,
-          :cache_l2 => 1.MiB
+          :vendor             => lookup('sol', node_uid, 'processor', 'vendor'),
+          :model              => lookup('sol', node_uid, 'processor', 'model'),
+          :version            => lookup('sol', node_uid, 'processor', 'version'),
+          :clock_speed        => lookup('sol', node_uid, 'processor', 'clock_speed'),
+          :instruction_set    => lookup('sol', node_uid, 'processor', 'instruction_set'),
+          :other_description  => lookup('sol', node_uid, 'processor', 'other_description'),
+          :cache_l1           => lookup('sol', node_uid, 'processor', 'cache_l1'),
+          :cache_l1i          => lookup('sol', node_uid, 'processor', 'cache_l1i'),
+          :cache_l1d          => lookup('sol', node_uid, 'processor', 'cache_l1d'),
+          :cache_l2           => lookup('sol', node_uid, 'processor', 'cache_l2'),
+          :cache_l3           => lookup('sol', node_uid, 'processor', 'cache_l3')
         })
+
         main_memory({
-          :ram_size => 4.GiB, # bytes
+          :ram_size     => lookup('sol', node_uid, 'main_memory', 'ram_size'),
           :virtual_size => nil
         })
+
         operating_system({
-          :name => "Debian",
-          :release => "Squeeze",
-          :version => "6.0",
-          :kernel => "2.6.32"
+          :name     => lookup('sol', node_uid, 'operating_system', 'name'),
+          :release  => "Squeeze",
+          :version  => lookup('sol', node_uid, 'operating_system', 'version'),
+          :kernel   => lookup('sol', node_uid, 'operating_system', 'kernel')
         })
+
         storage_devices [{
-          :interface => 'SATA',
-          :size =>  250.GB,
-          :device => "sda",
-          :model  => lookup('sol', node_uid, 'block_devices', 'sda', 'model'),
-          :driver => "sata_nv"
+          :interface  => 'SATA',
+          :size       => lookup('sol', node_uid, 'block_devices', 'sda', 'size'),
+          :driver     => "sata_nv",
+          :device     => lookup('sol', node_uid, 'block_devices', 'sda', 'device'),
+          :model      => lookup('sol', node_uid, 'block_devices', 'sda', 'model'),
+          :vendor     => lookup('sol', node_uid, 'block_devices', 'sda', 'vendor'),
+          :rev        => lookup('sol', node_uid, 'block_devices', 'sda', 'rev')
         }]
+
         network_adapters [{
-             :interface => 'Myrinet',
-             :rate => 10.G,
-             :network_address => "#{node_uid}-myri0.#{site_uid}.grid5000.fr",
-             :ip => lookup('sol', node_uid, 'network_interfaces','myri0', 'ip'),
-             :mac => nil,
-             :mac => lookup('sol', node_uid, 'network_interfaces','myri0', 'mac'),
-             :enabled => true,
-             :mounted => true,
-             :mountable => true,
-             :management => false,
-             :vendor => 'Myricom',
-             :version => "M3F-PCIXF-2",
-             :driver => "mx",
-             :switch => "sw-myrinet",
-             :device => 'myri0',
-             },
-            {
-            :interface => 'Ethernet',
-            :rate => 1.G,
-            :mac => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'mac'),
-            :ip => lookup('sol', node_uid, 'network_interfaces','eth0', 'ip'),
-            :vendor => 'nVidia',
-            :version => 'MCP55 Pro',
-            :enabled => true,
-            :management => false,
-            :mountable => true,
-            :bridged => true,
-            :driver => 'forcedeth',
-            :mounted => true,
-            :device => 'eth0',
-            :network_address => "#{node_uid}.#{site_uid}.grid5000.fr",
-            :switch => lookup('sol', node_uid, 'network_interfaces','eth0', 'switch_name'),
-            :switch_port => lookup('sol', node_uid, 'network_interfaces','eth0', 'switch_port')
-          },{
-            :interface => 'Ethernet',
-            :rate => 1.G,
-            :mac => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'mac'),
-            :vendor => 'nVidia',
-            :version => 'MCP55 Pro',
-            :enabled => false,
-            :driver => 'forcedeth',
-            :device => 'eth1'
-          },
-         # ,{
-         #   :interface => 'Ethernet',
-         #   :rate => 1.G,
-         #   :mac => nil,
-         #   :vendor => 'Broadcom',
-         #   :version => 'BCM5715c',
-         #   :enabled => false,
-         #   :driver => 'tg3'
-         # },{
-         #   :interface => 'Ethernet',
-         #   :rate => 1.G,
-         #   :mac => nil,
-         #   :vendor => 'Broadcom',
-         #   :version => 'BCM5715c',
-         #   :enabled => false,
-         #   :driver => 'tg3
-         # }
+          :interface        => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'interface'),
+          :rate             => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'rate'),
+          :enabled          => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'enabled'),
+          :management       => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'management'),
+          :mountable        => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'mountable'),
+          :mounted          => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'mounted'),
+          :bridged          => true,
+          :device           => "eth0",
+          :vendor           => 'nVidia',
+          :version          => "MCP55 Pro",
+          :driver           => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'driver'),
+          :network_address  => "#{node_uid}.#{site_uid}.grid5000.fr",
+          :ip               => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'ip'),
+          :ip6              => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'ip6'),
+          :switch           => "gw-sophia",
+          :switch_port      => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'switch_port'),
+          :mac              => lookup('sol', node_uid, 'network_interfaces', 'eth0', 'mac')
+        },
+        {
+          :interface        => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'interface'),
+          :rate             => 1.G,
+          :enabled          => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'enabled'),
+          :management       => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'management'),
+          :mountable        => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'mountable'),
+          :mounted          => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'mounted'),
+          :bridged          => false,
+          :device           => "eth1",
+          :vendor           => 'nVidia',
+          :version          => "MCP55 Pro",
+          :driver           => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'driver'),
+          :ip               => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'ip'),
+          :mac              => lookup('sol', node_uid, 'network_interfaces', 'eth1', 'mac')
+        },
+        {
+          :interface => 'Ethernet',
+          :rate => 100.M,
+          :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
+          :ip => lookup('sol', node_uid, 'network_interfaces','bmc', 'ip'),
+          :mac => lookup('sol', node_uid, 'network_interfaces','bmc', 'mac'),
+          :enabled  => true,
+          :mounted => false,
+          :mountable => false,
+          :management => true,
+          :device => "bmc"
+        },
+        {
+          :interface            => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'interface'),
+          :rate                 => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'rate'),
+          :network_address      => "#{node_uid}-myri0.#{site_uid}.grid5000.fr",
+          :ip                   => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'ip'),
+          :ip6                  => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'ip6'),
+          :mac                  => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'mac'),
+          :vendor               => 'Myricom',
+          :version              => "M3F-PCIXF-2",
+          :driver               => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'driver'),
+          :enabled              => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'enabled'),
+          :management           => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'management'),
+          :mountable            => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'mountable'),
+          :mounted              => lookup('sol', node_uid, 'network_interfaces', 'myri0', 'mounted'),
+          :management           => false,
+          :device               => "myri0",
+          :switch               => 'sw-myrinet'
+        }]
 
-          {
-            :interface => 'Ethernet',
-            :rate => 100.M,
-            :network_address  => "#{node_uid}-bmc.#{site_uid}.grid5000.fr",
-            :ip => lookup('sol', node_uid, 'network_interfaces','bmc', 'ip'),
-            :mac => lookup('sol', node_uid, 'network_interfaces','bmc', 'mac'),
-            :enabled  => true,
-            :mounted => false,
-            :mountable => false,
-            :management => true,
-            :device => "bmc"
-          }]
-          bios({
-            :version => lookup('sol', node_uid, 'network_interfaces','version'),
-             :vendor => lookup('sol', node_uid, 'network_interfaces','vendor'),
-            :release_date => lookup('sol', node_uid, 'network_interfaces','release_date')
-          })
-          chassis({
-            :serial => lookup('sol', node_uid, 'chassis','serial_number'),
-            :name => lookup('sol', node_uid, 'chassis','product_name')
-          })
-          gpu({
-            :gpu  => false
-            })
+        chassis({
+          :serial       => lookup('sol', node_uid, 'chassis', 'serial_number'),
+          :name         => lookup('sol', node_uid, 'chassis', 'product_name'),
+          :manufacturer => lookup('sol', node_uid, 'chassis', 'manufacturer')
+        })
 
-          monitoring({
-            :wattmeter  => false
-          })
+        bios({
+          :version      => lookup('sol', node_uid, 'bios', 'version'),
+          :vendor       => lookup('sol', node_uid, 'bios', 'vendor'),
+          :release_date => lookup('sol', node_uid, 'bios', 'release_date')
+        })
+
+        gpu({
+          :gpu  => false
+        })
+
+        monitoring({
+          :wattmeter  => false
+        })
       end
     end
   end
