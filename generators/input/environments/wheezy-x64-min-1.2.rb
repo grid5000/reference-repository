0fc332822ea22c71c17b21b6f0fc49e42b8595a3
Émile Morel <emile.morel@inria.fr>
2013-08-23 13:40:55 +0200

    [Environments] Added Environment wheezy-x64-min version 8 and squeeze-x64-min version 9 to the API

---
+++ generators/input/environments/wheezy-x64-min-1.2.rb
@@ -0,0 +1,14 @@
+environment 'wheezy-x64-min-1.2' do
+  state "stable"
+  file({:path => "/grid5000/images/wheezy-x64-min-1.2.tgz", :md5 => "e058b74f44c8c23864577abd4be7d518"})
+  kernel "3.2.0-4"
+  available_on %w{grenoble lille luxembourg lyon nancy reims rennes sophia toulouse}
+  valid_on "adonis , edel , genepi , chicon , chimint , chinqchint , chirloute , granduc , hercule, orion, sagittaire, taurus, graphene , griffon , stremi , paradent , paramount , parapide , parapluie , helios , sol , suno, pastel"
+  based_on "Debian version wheezy for amd64"
+  consoles [{:port => "ttyS0", :bps => 34800}]
+  services []
+  accounts [{:login => "root", :password => "grid5000"}]
+  applications %w{Vim nano Perl}
+  x11_forwarding true
+  tcp_bandwidth 1.G
+end
