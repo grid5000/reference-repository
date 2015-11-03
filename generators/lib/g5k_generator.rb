b6e849bf1b6e27a8932f4bf0d87f0d25113fde90
Simon Delamare <simon.delamare@ens-lyon.fr>
2015-11-03 10:17:47 +0100

    [toulouse] Toulouse is closing, remove all data

--- generators/lib/g5k_generator.rb
+++ generators/lib/g5k_generator.rb
@@ -207,7 +207,7 @@ module G5K
 
     # This method is used exclusivly for environments. Example:
     # environment 'squeeze-x64-xen-0.8' do
-    #  available_on %w{grenoble lille lyon nancy rennes sophia toulouse}
+    #  available_on %w{grenoble lille lyon nancy rennes sophia}
     # end    
     def available_on(sites_uid)
       env_uid = @context[:uid]
