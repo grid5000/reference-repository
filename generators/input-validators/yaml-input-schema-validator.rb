2978a3bfcca31be984b687c057dc5f48fc43f796
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2016-09-28 14:31:05 +0200

    yaml-input-schema-validator: Add support for selecting site/cluster

--- generators/input-validators/yaml-input-schema-validator.rb
+++ generators/input-validators/yaml-input-schema-validator.rb
@@ -23,7 +23,7 @@ def run_validator(uid, data, schema)
   return true
 end
 
-def yaml_input_schema_validator(global_hash)
+def yaml_input_schema_validator(global_hash, sites = nil, clusters = nil)
   dir = Pathname(__FILE__).parent
 
   schema_global  = load_yaml_schema("#{dir}/schema-global.yaml")
@@ -34,10 +34,12 @@ def yaml_input_schema_validator(global_hash)
   r &= run_validator('global', global_hash, schema_global) #
   
   global_hash["sites"].each do |site_uid, site|
+    next if sites and not sites.include?(site_uid)
     
     r &= run_validator(site_uid, site, schema_site) #
     
     site["clusters"].each do |cluster_uid, cluster|
+      next if clusters and not clusters.include?(cluster_uid)
       
       r &= run_validator(cluster_uid, cluster, schema_cluster) #
       
@@ -52,8 +54,36 @@ def yaml_input_schema_validator(global_hash)
 end
 
 if __FILE__ == $0
+  require 'optparse'
+
+  options = {}
+
+  OptionParser.new do |opts|
+    opts.banner = "Usage: yaml-input-schema-validator.rb [options]"
+    
+    ###
+    
+    opts.separator ""
+    opts.separator "Filters:"
+    
+    opts.on('-s', '--sites a,b,c', Array, 'Select site(s)',
+            "Default: all sites") do |s|
+      options[:sites] = s
+    end
+    
+    opts.on('-c', '--clusters a,b,c', Array, 'Select clusters(s). Default: all') do |s|
+      options[:clusters] = s
+    end
+    
+    # Print an options summary.
+    opts.on_tail("-h", "--help", "Show this message") do
+      puts opts
+      exit
+    end
+  end.parse!
+
   puts "Checking input data:\n\n"
   global_hash = load_yaml_file_hierarchy("#{dir}/../../input/grid5000/")
-  r = yaml_input_schema_validator(global_hash)
+  r = yaml_input_schema_validator(global_hash, options[:sites], options[:clusters])
   puts 'OK' if r
 end
