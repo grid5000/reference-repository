b28398702489cc7e7e18c714fbc0c0af7c975f6d
Nicolas Michon <nicolas.michon@inria.fr>
2016-10-24 18:01:04 +0200

    [dev] correct return codes of scripts used by jenkins tests

--- generators/input-validators/yaml-input-schema-validator.rb
+++ generators/input-validators/yaml-input-schema-validator.rb
@@ -30,23 +30,23 @@ def yaml_input_schema_validator(global_hash, sites = nil, clusters = nil)
   schema_site    = load_yaml_schema("#{dir}/schema-site.yaml")
   schema_cluster = load_yaml_schema("#{dir}/schema-cluster.yaml")
   schema_node    = load_yaml_schema("#{dir}/schema-node.yaml")
-  
+
+  r = true
+
   r &= run_validator('global', global_hash, schema_global) #
-  
+
   global_hash["sites"].each do |site_uid, site|
     next if sites and not sites.include?(site_uid)
-    
+
     r &= run_validator(site_uid, site, schema_site) #
-    
+
     site["clusters"].each do |cluster_uid, cluster|
       next if clusters and not clusters.include?(cluster_uid)
-      
+
       r &= run_validator(cluster_uid, cluster, schema_cluster) #
-      
+
       cluster["nodes"].each do |node_uid, node|
-        
         r &= run_validator(node_uid, node, schema_node) #
-        
       end
     end
   end
@@ -60,21 +60,21 @@ if __FILE__ == $0
 
   OptionParser.new do |opts|
     opts.banner = "Usage: yaml-input-schema-validator.rb [options]"
-    
+
     ###
-    
+
     opts.separator ""
     opts.separator "Filters:"
-    
+
     opts.on('-s', '--sites a,b,c', Array, 'Select site(s)',
             "Default: all sites") do |s|
       options[:sites] = s
     end
-    
+
     opts.on('-c', '--clusters a,b,c', Array, 'Select clusters(s). Default: all') do |s|
       options[:clusters] = s
     end
-    
+
     # Print an options summary.
     opts.on_tail("-h", "--help", "Show this message") do
       puts opts
@@ -86,4 +86,5 @@ if __FILE__ == $0
   global_hash = load_yaml_file_hierarchy("#{dir}/../../input/grid5000/")
   r = yaml_input_schema_validator(global_hash, options[:sites], options[:clusters])
   puts 'OK' if r
+  exit r
 end
