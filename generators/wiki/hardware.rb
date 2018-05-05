a7009ae1206a8023c24b1912756dd13a96235862
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-05-05 15:06:02 +0200

    Add Skylake to list of microarchs

--- generators/wiki/hardware.rb
+++ generators/wiki/hardware.rb
@@ -204,6 +204,7 @@ class G5KHardwareGenerator < WikiGenerator
       'Sandy Bridge' => '2012',
       'Haswell' => '2013',
       'Broadwell' => '2015',
+      'Skylake' => '2016'
     }
     date = release_dates[microarchitecture.to_s]
     raise "ERROR: microarchitecture not found: '#{microarchitecture.to_s}'. Add in hardware.rb" if date.nil?
