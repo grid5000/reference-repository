6d7bf167b70d477301be12227e223da54dbf4867
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-07-05 13:37:48 +0200

    wikigen: add some context when diffing

--- generators/wiki/lib/wiki_generator.rb
+++ generators/wiki/lib/wiki_generator.rb
@@ -68,7 +68,7 @@ class WikiGenerator
   def diff_page()
     wiki_content = remove_page_creation_date(@mw_client.get_page_content(@page_name)).strip # .strip removes potential '\n' at end of file
     generated_content = remove_page_creation_date(@generated_content).strip
-    diff = Diffy::Diff.new(wiki_content, generated_content, :context => 0)
+    diff = Diffy::Diff.new(wiki_content, generated_content, :context => 3)
     if (diff.to_s.empty?)
       puts "No differences found between generated and current wiki content for page #{@page_name}."
       return true
