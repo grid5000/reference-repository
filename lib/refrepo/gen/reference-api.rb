317157b58d64ddeaa8040b99b8880e2ae4980542
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-12-07 13:23:03 +0100

    [dev/gen/ref-api] Add some missing sort keys

--- lib/refrepo/gen/reference-api.rb
+++ lib/refrepo/gen/reference-api.rb
@@ -231,10 +231,10 @@ def generate_reference_api
 
           # Convert hashes to arrays
           Hash(node["storage_devices"]).each { |key, hash| node["storage_devices"][key]["device"] = key; } # Add "device: sdX" within the hash
-          node["storage_devices"] = Hash(node["storage_devices"]).sort_by_array(["sda", "sdb", "sdc", "sdd", "sde"]).values
+          node["storage_devices"] = Hash(node["storage_devices"]).sort_by_array(["sda", "sdb", "sdc", "sdd", "sde", "sdf", "nvme0n1", "nvme1n1"]).values
 
           node["network_adapters"].each { |key, hash| node["network_adapters"][key]["device"] = key; } # Add "device: ethX" within the hash
-          node["network_adapters"] = node["network_adapters"].sort_by_array(["eth0", "eth1", "eth2", "eth3", "eth4", "eth5", "eth6", "ib0", "ib1", "ib2", "ib3", "bmc"]).values
+          node["network_adapters"] = node["network_adapters"].sort_by_array(["eth0", "eth1", "eth2", "eth3", "eth4", "eth5", "eth6", "ib0.8100", "ib0", "ib1", "ib2", "ib3", "bmc"]).values
 
           # For each network adapters, populate "network_address", "switch" and "switch_port" from the network equipment description
           node["network_adapters"].each { |network_adapter|
