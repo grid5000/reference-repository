5d812f7ca823950a59736aa4f4a0c9bc8acd2444
Pierre Neyron <pierre.neyron@imag.fr>
2019-04-19 12:49:56 +0200

    Revert "make 1 the default verbosity level"
    This reverts commit e05e4432c1c34266eb093f4601eac4f578bcedf3.

--- lib/refrepo/gen/oar-properties.rb
+++ lib/refrepo/gen/oar-properties.rb
@@ -665,7 +665,7 @@ def generate_oar_properties(options)
           end
 
           case options[:verbose]
-          when 0..1
+          when 1
             puts "#{key}:#{info}" if info != ''
             puts "#{key}:#{diff_keys}" if diff.size != 0
           when 2
