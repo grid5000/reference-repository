e877a849e2afdea42925de0c321241a4d5735f16
Patrice RINGOT <patrice.ringot@loria.fr>
2019-08-07 18:02:49 +0200

    [wikigen] use the new GPU representation to generate wiki informations

--- lib/refrepo/gen/wiki/generators/site_hardware.rb
+++ lib/refrepo/gen/wiki/generators/site_hardware.rb
@@ -1,5 +1,47 @@
 # coding: utf-8
 
+class GPURef
+  @@gpu2cores = {
+    "GeForce RTX 2080 Ti" => 4352,
+    "GeForce GTX 1080 Ti" => 3584,
+    "Tesla P100-PCIE-16GB" => 3584,
+    "Tesla V100-PCIE-32GB" => 5120,
+    "Tesla M2075" => 448,
+    "GeForce GTX TITAN Black" => 2880,
+    "GeForce GTX 980" => 2048,
+    "Tesla K40m" => 2880,
+  }
+
+  @@new_gpu_names2old_ones = {
+    "GeForce RTX 2080 Ti" => "RTX 2080 Ti",
+    "GeForce GTX 1080 Ti" => "GTX 1080 Ti",
+    "Tesla P100-PCIE-16GB" => "Tesla P100",
+    "Tesla V100-PCIE-32GB" => "Tesla V100",
+    "Tesla M2075" => "Tesla M2075",
+    "GeForce GTX TITAN Black" => "Titan Black",
+    "GeForce GTX 980" => "GTX 980",
+    "Tesla K40m" => "Tesla K40M",
+  }
+
+  def self.getNumberOfCoresFor(model)
+    if @@gpu2cores[model]
+      return @@gpu2cores[model]
+    else
+      raise "Fix me: #{model} is missing"
+    end
+  end
+
+  # will not keep this, just to ease manual testing for bug #10436
+  def self.getGrid5000LegacyNameFor(model)
+    if @@new_gpu_names2old_ones[model]
+      return @@new_gpu_names2old_ones[model]
+    else
+      return model
+    end
+  end
+
+end
+
 class SiteHardwareGenerator < WikiGenerator
 
   def initialize(page_name, site)
@@ -316,9 +358,25 @@ def get_hardware(sites)
           s
         end.join('<br />')
 
-        gpu = node_hash['gpu']
-        hard['gpu_str'] = if gpu && gpu['gpu']
-                            (gpu['gpu_count'].to_i == 1 ? '' : gpu['gpu_count'].to_s + '&nbsp;x&nbsp;') + gpu['gpu_vendor'].to_s + ' ' + gpu['gpu_model'].to_s.gsub(' ', '&nbsp;')
+        lgpu = node_hash['gpu_devices']
+        hard['gpu_str'] = if lgpu
+                            bymodel = {}
+                            lgpu.each { |g|
+                              d = g[1]
+                              vendor = d['vendor']
+                              model = GPURef.getGrid5000LegacyNameFor(d['model'])
+                              vm = vendor.to_s + ' ' + model.to_s.gsub(' ', '&nbsp;')
+                              if bymodel[vm]
+                                bymodel[vm] += 1
+                              else
+                                bymodel[vm] = 1
+                              end
+                            }
+                            res = []
+                            bymodel.each { |model,count|
+                              res << (count == 1 ? '' : count.to_s + '&nbsp;x&nbsp;') + model
+                            }
+                            res.join(", ")
                           else
                             ''
                           end
