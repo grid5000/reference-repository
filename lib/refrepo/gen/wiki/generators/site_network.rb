f4f6c54bf3430620a6e90056298792a7af110a2f
David Loup <david.loup@inria.fr>
2020-02-20 11:31:28 +0100

    Add warning when generating network diagram about graphviz version (bug 11470)

--- lib/refrepo/gen/wiki/generators/site_network.rb
+++ lib/refrepo/gen/wiki/generators/site_network.rb
@@ -34,6 +34,7 @@ class SiteNetworkGenerator < WikiGenerator
     @generated_content += MW::LINE_FEED
 
     # this will generate dot and png network maps in the current directory
+    puts "WARNING : Graph generation depends on graphviz version, check the graphs before uploading it. graphviz 2.40.1-6 seems to work correctly"
     check_network_description({:sites => [@site], :dot => true})
   end
 end
