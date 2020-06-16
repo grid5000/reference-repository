fba47df19bdb01e7dd258326ca1242e4b32443a6
David Loup <david.loup@inria.fr>
2020-06-16 15:30:15 +0200

    [gen/wiki/oar_properties] Add 'exotic' propertie to OAR propertie wiki

--- lib/refrepo/gen/wiki/generators/oar_properties.rb
+++ lib/refrepo/gen/wiki/generators/oar_properties.rb
@@ -56,6 +56,11 @@ class OarPropertiesGenerator < WikiGenerator
     "diskpath" => {
       "description" => "Device path of a reservable disk on a node, for resources of type 'disk'.",
     },
+    "exotic" => {
+      "description" => "Resource has 'atypical' hardware (such as non-x86_64 CPU architecture for example)",
+      "value_type" => "Boolean",
+      "possible_values" => "YES, NO"
+    },
     "gpu" => {
       "description" => "The ID of the GPU the resource is part of. The unique scope is the OAR server. ",
       "possible_values" => "1, 2, 3, ..."
@@ -218,7 +223,7 @@ class OarPropertiesGenerator < WikiGenerator
     "Job-related properties" => ["besteffort", "deploy", "production", "cluster_priority", "max_walltime"],
     "Hierarchy" => ["chassis", "cluster", "cpu", "cpuset", "core", "disk", "diskpath", "gpu", "gpudevice", "host", "slash_16", "slash_17", "slash_18", "slash_19", "slash_20", "slash_21", "slash_22", "switch", "subnet_address", "subnet_prefix", "vlan"],
     "Hardware" => ["gpu_model", "gpu_count", "memnode", "memcore", "memcpu", "disktype", "disk_reservation_count", "myri_rate", "myri_count", "myri", "ib_rate", "ib_count", "ib", "opa_rate", "opa_count", "eth_rate", "eth_count", "cpufreq", "cputype", "cpucore", "cpuarch", "virtual", "mic"],
-    "Miscellaneous" => ["wattmeter", "nodemodel", "network_address", "ip", "type", "expiry_date", "comment", "maintenance"]
+    "Miscellaneous" => ["wattmeter", "nodemodel", "network_address", "ip", "type", "expiry_date", "comment", "maintenance", "exotic"]
   }
 
   #Existing properties that won't be documented
