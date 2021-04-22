9a174cda0f63bf2dd0a56b068a0efea62d56ec97
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2021-04-22 21:08:14 +0200

    [spec] move arguments tests to a separate file

--- spec/oar_properties_spec.rb
+++ spec/oar_properties_spec.rb
@@ -5,60 +5,6 @@ conf = RefRepo::Utils.get_api_config
 
 describe 'OarProperties' do
 
-  context 'testing arguments' do
-    before do
-      prepare_stubs("dump_oar_api_empty_server.json", "load_data_hierarchy_stubbed_data.json")
-    end
-
-    it 'should should accept case where only the site is specified' do
-
-      uri = URI(conf["uri"])
-
-      response = Net::HTTP.get(uri)
-
-      expect(response).to be_an_instance_of(String)
-
-      options = {
-          :table => true,
-          :print => false,
-          :update => false,
-          :diff => false,
-          :site => "fakesite",
-      }
-
-      expected_header = <<-TXT
-+---------- + -------------------- + ----- + ----- + -------- + ---- + -------------------- + ------------------------------ + ------------------------------+
-|   cluster | host                 | cpu   | core  | cpuset   | gpu  | gpudevice            | cpumodel                       | gpumodel                      |
-+---------- + -------------------- + ----- + ----- + -------- + ---- + -------------------- + ------------------------------ + ------------------------------+
-TXT
-
-      generator_output = capture do
-        generate_oar_properties(options)
-      end
-
-      expect(generator_output[:stdout]).to include(expected_header)
-    end
-
-    it 'should should accept case where only the site is specified' do
-
-      uri = URI(conf["uri"])
-
-      response = Net::HTTP.get(uri)
-
-      expect(response).to be_an_instance_of(String)
-
-      options = {
-          :table => true,
-          :print => false,
-          :update => false,
-          :diff => false,
-          :site => "fakesite2",
-      }
-
-      expect { generate_oar_properties(options) }.to raise_error("The provided site does not exist : I can't detect clusters")
-    end
-  end
-
   context 'interracting with an empty OAR server' do
     before do
       prepare_stubs("dump_oar_api_empty_server.json", "load_data_hierarchy_stubbed_data.json")
