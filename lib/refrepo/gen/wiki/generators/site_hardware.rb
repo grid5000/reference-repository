grid5000/reference-repository

commit 3fd6236ef11409a5fdbc63b474063793f2ca7fb3

[gen] [wiki] [site_hardware] Refactor generation of network description

lib/refrepo/gen/wiki/generators/site_hardware.rb
@@ -386,33 +386,33 @@ def get_hardware(sites)
         }
         nic_c = 0
         hard['network_description'] = network_description.map do |e|
-          s  = e['count'] > 1 ? "\n* " : ''
-          s += e['unavailable_for_experiment'] ? '<span style="color:grey">' : ''
+          desc = []
           if e['name'].nil? or e['name'] == e['device']
-            s += e['device']
+            desc.append(e['device'])
           else
-            s +=  e['device'] + "/" + e['name']
+            desc.append(e['device'] + "/" + e['name'])
           end
-          s += ', '
-          s += e['interface']
-          s += ', '
+          desc.append(e['interface'])
+
           if !(e['unwired'] and e['unavailable_for_experiment'])
-            s += 'configured rate: ' + (e['unwired'] ? 'n/c' : G5K.get_rate(e['rate']))
-            s += ', '
+            desc.append('configured rate: ' + (e['unwired'] ? 'n/c' : G5K.get_rate(e['rate'])))
           end
           if !(e['model'] == 'N/A N/A' and e['unavailable_for_experiment']) # don't include interface model if not available
             e['model'] = 'N/A' if e['model'] == 'N/A N/A'
-            s += 'model: '+ e['model'] + ', '
+            desc.append('model: '+ e['model'])
           end
-          s +=  'driver: ' + e['driver'] if e['driver']
-          if e['unavailable_for_experiment']
-            s += ' - unavailable for experiment'
+          desc.append('driver: ' + e['driver']) if e['driver']
+          # Generate final string and then adjust
+          s = desc.join(', ')
+          if e['no_kavlan']
+            s += ' - no KaVLAN'
+          elsif e['unavailable_for_experiment']
+            s = '<span style="color:grey">' + s + ' - unavailable for experiment</span>'
           elsif e['device'] =~ /eth/
-            s += ' [[Advanced_KaVLAN#A_simple_multi_NICs_example|(multi NICs example)]]' if !nic_c.zero? and not e.fetch('no_kavlan')
+            s += ' [[Advanced_KaVLAN#A_simple_multi_NICs_example|(multi NICs example)]]' if !nic_c.zero?
             nic_c += 1
           end
-          s += ' - no KaVLAN' if e['no_kavlan']
-          s +=  e['unavailable_for_experiment'] ? '</span>' : ''
+          s = "\n* " + s
           s
         end.join('<br />')
 

commit 15f05a99bb132e4261b7462a08984ac6f8c5d13c

[gen][wiki][site_hardware] replace .append with '<<' to add in Array
The append method works fine with recent version of Ruby, however, we
still need to support old version of Ruby (2.1) to work with our Jenkins
tests suite.
Fixes: 3fd6236ef11409a5fdbc63b474063793f2ca7fb3

lib/refrepo/gen/wiki/generators/site_hardware.rb
@@ -391,21 +391,21 @@ def get_hardware(sites)
         hard['network_description'] = network_description.map do |e|
           desc = []
           if e['name'].nil? or e['name'] == e['device']
-            desc.append(e['device'])
+            desc << e['device']
           else
-            desc.append(e['device'] + "/" + e['name'])
+            desc << e['device'] + "/" + e['name']
           end
-          desc.append(e['interface'])
+          desc << e['interface']
 
           if !(e['unwired'] and e['unavailable_for_experiment'])
-            desc.append('configured rate: ' + (e['unwired'] ? 'n/c' : G5K.get_rate(e['rate'])))
+            desc << 'configured rate: ' + (e['unwired'] ? 'n/c' : G5K.get_rate(e['rate']))
           end
           if !(e['model'] == 'N/A N/A' and e['unavailable_for_experiment']) # don't include interface model if not available
             e['model'] = 'N/A' if e['model'] == 'N/A N/A'
-            desc.append('model: '+ e['model'])
+            desc << 'model: '+ e['model']
           end
-          desc.append('driver: ' + e['driver']) if e['driver']
-          desc.append('SR-IOV enabled') if e['sriov'] and not e['unavailable_for_experiment']
+          desc << 'driver: ' + e['driver'] if e['driver']
+          desc << 'SR-IOV enabled' if e['sriov'] and not e['unavailable_for_experiment']
           # Generate final string and then adjust
           s = desc.join(', ')
           if e['no_kavlan']