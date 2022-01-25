940d9e75478a5343ffadcf4839f41bf1440640b7
Samir Noir <samir.noir@inria.fr>
2022-01-25 17:39:17 +0100

    [gen/oarsub-simplifier-aliases] sort the clusters and nodes aliases

--- lib/refrepo/gen/puppet/oarsub-simplifier-aliases.rb
+++ lib/refrepo/gen/puppet/oarsub-simplifier-aliases.rb
@@ -55,6 +55,7 @@ def generate_all_sites_aliases
       aliases[site][cluster] = "cluster='#{cluster}'"
       aliases[site]["#{cluster}-%d"] = "host='#{cluster}-%d.#{site}.grid5000.fr'"
     end
+    aliases[site] = aliases[site].sort_by { |site, _| site }.to_h
   end
 
   aliases = aliases.sort_by { |site, _| site }.to_h
