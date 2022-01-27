ce8e59b9f02018a09f6710f50f35d8303214d193
Matthieu Imbert <matthieu.imbert@inria.fr>
2022-01-27 11:29:32 +0100

    [gen] kavlanng: misc fixes for rubocop

--- lib/refrepo/gen/puppet/kavlanngg5k.rb
+++ lib/refrepo/gen/puppet/kavlanngg5k.rb
@@ -16,11 +16,11 @@ def gen_json(output_path)
     site_h['clusters'].each do |cluster_id, cluster_h|
       puts '  ' + cluster_id
       cluster_h.delete_if { |k| k != 'nodes' }
-      cluster_h['nodes'].each do |node_id, node_h|
+      cluster_h['nodes'].each do |_node_id, node_h|
         node_h.delete_if { |k| k != 'network_adapters' }
       end
     end
-    site_h['network_equipments'].delete_if { |k, v| v['kind'] != 'router' }
+    site_h['network_equipments'].delete_if { |_k, v| v['kind'] != 'router' }
     routers = site_h['network_equipments'].keys
     if routers.length != 1
       puts "ERROR: #{site_id} has #{routers.length} routers"
@@ -28,21 +28,21 @@ def gen_json(output_path)
     gw = routers[0]
     puts "  #{site_id} gw: #{gw}"
     site_h['network_equipments'][gw].delete_if { |k| ! ['ip', 'ip6', 'kind'].include? k }
-    site_h['servers'].delete_if { |k, v| k != 'dns' }
+    site_h['servers'].delete_if { |k, _v| k != 'dns' }
     dns_list = site_h['servers'].keys
     if dns_list.length != 1
       puts "ERROR: #{site_id} has #{dns_list.length} DNS"
     end
     begin
-      site_h['servers']['dns'].delete_if { |k, v| k != 'network_adapters' }
-      site_h['servers']['dns']['network_adapters'].delete_if { |k, v| k != 'default' }
+      site_h['servers']['dns'].delete_if { |k, _v| k != 'network_adapters' }
+      site_h['servers']['dns']['network_adapters'].delete_if { |k, _v| k != 'default' }
     rescue
       puts "ERROR: #{site_id} unable to properly clean DNS server entry"
     end
     begin
       dns_ip = site_h['servers']['dns']['network_adapters']['default']['ip']
       puts "  #{site_id} DNS IP address: #{dns_ip}"
-    rescue Exception => e
+    rescue StandardError
       puts "ERROR: #{site_id} unable to get DNS IP address"
     end
   end
