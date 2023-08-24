98054671b77060db63f296b57c4378afe9fe9404
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2023-08-24 11:46:33 +0200

    URI.open() must be called explicitely (not just open()) with Ruby from bookworm

--- lib/refrepo/utils.rb
+++ lib/refrepo/utils.rb
@@ -15,7 +15,7 @@ module RefRepo::Utils
       o = {}
       puts "Check the documentation https://www.grid5000.fr/w/TechTeam:Reference_Repository#Credentials and your ~/.grid5000_api.yml file"
     end
-    d = open("#{conf['uri']}/#{conf['version']}/#{path}", o).read
+    d = URI.open("#{conf['uri']}/#{conf['version']}/#{path}", o).read
     return JSON::parse(d)
   end
 
