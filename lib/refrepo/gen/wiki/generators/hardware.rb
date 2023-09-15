54434199827482a86e99b14cb7b5336bbac7fd62
Pierre Neyron <pierre.neyron@imag.fr>
2023-09-15 23:29:42 +0200

    [wiki:hw] Rework the processor section: add per arch + all per core

--- lib/refrepo/gen/wiki/generators/hardware.rb
+++ lib/refrepo/gen/wiki/generators/hardware.rb
@@ -20,7 +20,10 @@ class G5KHardwareGenerator < WikiGenerator
 
   def generate_totals
     data = {
+      'proc_architectures' => {},
+      'core_architectures' => {},
       'proc_families' => {},
+      'core_families' => {},
       'proc_models' => {},
       'core_models' => {},
       'ram_size' => {},
@@ -45,6 +48,7 @@ class G5KHardwareGenerator < WikiGenerator
             vendor = node_hash['processor']['vendor']
             model = node_hash['processor']['model'].gsub(/^#{vendor} /, '')
             version = node_hash['processor']['version']
+            architecture = node_hash['architecture']['platform_type']
             proc_desc = "#{model}"
             if version != 'Unknown'
               proc_desc += " #{version}"
@@ -54,11 +58,21 @@ class G5KHardwareGenerator < WikiGenerator
             cluster_procs = node_hash['architecture']['nb_procs']
             cluster_cores = node_hash['architecture']['nb_cores']
 
-            key = [vendor, model]
+            key = [architecture]
+            init(data, 'proc_architectures', key)
+            data['proc_architectures'][key][site_uid] += cluster_procs
+
+            init(data, 'core_architectures', key)
+            data['core_architectures'][key][site_uid] += cluster_cores
+
+            key = [vendor, model, architecture]
             init(data, 'proc_families', key)
             data['proc_families'][key][site_uid] += cluster_procs
 
-            key = [vendor, {text: proc_desc, sort: get_date(microarchitecture) + ', ' + proc_desc.to_s}, {text: microarchitecture || ' ', sort: get_date(microarchitecture) + ', ' + microarchitecture.to_s}]
+            init(data, 'core_families', key)
+            data['core_families'][key][site_uid] += cluster_cores
+
+            key = [vendor, {text: proc_desc, sort: get_date(microarchitecture) + ', ' + proc_desc.to_s}, {text: microarchitecture || ' ', sort: get_date(microarchitecture) + ', ' + microarchitecture.to_s}, architecture]
             init(data, 'proc_models', key)
             data['proc_models'][key][site_uid] += cluster_procs
 
@@ -254,17 +268,26 @@ class G5KHardwareGenerator < WikiGenerator
     }
 
     # Table construction
-    generated_content = "= Processors ="
-    generated_content += "\n== Processors counts per families ==\n"
     sites = @site_uids.map{ |e| "[[#{e.capitalize}:Hardware|#{e.capitalize}]]" }
     table_options = 'class="wikitable sortable" style="text-align: center;"'
-    table_columns = ['Vendor', 'Processor family'] + sites + ['Processors total']
+    generated_content = "= Processors ="
+    generated_content += "\n== Processors counts per architecture ==\n"
+    table_columns = ['Processor arch'] + sites + ['Processors total']
+    generated_content += MW.generate_table(table_options, table_columns, get_table_data(data, 'proc_architectures'))
+    generated_content += "\n== Cores counts per architecture ==\n"
+    table_columns = ['Core arch'] + sites + ['Core total']
+    generated_content += MW.generate_table(table_options, table_columns, get_table_data(data, 'core_architectures'))
+    generated_content += "\n== Processors counts per family ==\n"
+    table_columns = ['Vendor', 'Processor family', 'Arch'] + sites + ['Processors total']
     generated_content += MW.generate_table(table_options, table_columns, get_table_data(data, 'proc_families'))
-    generated_content += "\n== Processors counts per models ==\n"
-    table_columns = ['Vendor', 'Processor model', 'Microarchitecture'] + sites + ['Processors total']
+    generated_content += "\n== Cores counts per family ==\n"
+    table_columns = ['Vendor', 'Core family', 'Arch'] + sites + ['Core total']
+    generated_content += MW.generate_table(table_options, table_columns, get_table_data(data, 'core_families'))
+    generated_content += "\n== Processors counts per model ==\n"
+    table_columns = ['Vendor', 'Processor model', 'Microarch', 'Arch'] + sites + ['Processors total']
     generated_content += MW.generate_table(table_options, table_columns, get_table_data(data, 'proc_models'))
-    generated_content += "\n== Cores counts per models ==\n"
-    table_columns =  ['Vendor', 'Core model', 'Microarchitecture'] + sites + ['Cores total']
+    generated_content += "\n== Cores counts per model ==\n"
+    table_columns =  ['Vendor', 'Core model', 'Microarch', 'Arch'] + sites + ['Cores total']
     generated_content += MW.generate_table(table_options, table_columns, get_table_data(data, 'core_models'))
 
     generated_content += "\n= Memory =\n"
