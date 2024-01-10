41b56334c0fe3ecd40269506821210178091def4
Lucas Nussbaum <lucas.nussbaum@inria.fr>
2024-01-10 21:04:56 +0100

    [spec] Add output files for abacus22 case (before change)

--- spec/oar_properties2_spec.rb
+++ spec/oar_properties2_spec.rb
@@ -41,6 +41,11 @@ describe 'OarProperties2' do
     check_oar_properties({ :oar => 'oar_grue-without-gpus', :data => 'data_grue', :case => 'grue_nogpus' })
   end
 
+  it 'works on a cluster with more CPUs than GPUs (2 CPU, 1 GPU) such as abacus22' do
+    check_oar_properties({ :oar => 'oar_empty', :data => 'data_abacus22_2cpu1gpu', :case => 'abacus22_2cpu1gpu' })
+  end
+
+
   it 'generates an identical output for graffiti when all GPUs are there' do
     check_oar_properties({ :oar => 'oar_graffiti-with-all-gpus', :data => 'data_graffiti2', :case => 'graffiti-all-gpus' })
   end
