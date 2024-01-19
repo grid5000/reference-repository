4cd8ece816a3208aa5038473dc15710523af182a
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2024-01-19 09:18:10 +0100

    [gen:puppet] set warranty to false if information has not been declared

--- lib/refrepo/gen/puppet/clusters.rb
+++ lib/refrepo/gen/puppet/clusters.rb
@@ -19,9 +19,8 @@ def generate_puppet_clusters(options)
             _, f_node = c_hash["nodes"].first
             queue = f_node['supported_job_types']['queues'].sort.reverse.first
             disk_reservation = f_node['storage_devices'].filter{|d| d.key?('reservation')}.length > 0
-            gpu = f_node.key?('gpu_devices')
-            
-            warrantied = DateTime.parse(f_node['chassis']['warranty_end']) > DateTime.now
+            gpu = f_node.key?('gpu_devices')             
+            warrantied = f_node['chassis'].key?('warranty_end') ? DateTime.parse(f_node['chassis']['warranty_end']) > DateTime.now : false
 
             yaml_data[s_uid][c_uid] = {"queue" => queue, 
                                        "disk_reservation" => disk_reservation,
