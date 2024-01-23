994228e38b5570fb8962185e5cb8afa86b693172
Laurent Pouilloux <laurent.pouilloux@inria.fr>
2024-01-23 13:35:36 +0100

    [warranty] handle case when warranty information is missing for a cluster

--- lib/refrepo/gen/puppet/clusters.rb
+++ lib/refrepo/gen/puppet/clusters.rb
@@ -19,8 +19,13 @@ def generate_puppet_clusters(options)
             _, f_node = c_hash["nodes"].first
             queue = f_node['supported_job_types']['queues'].sort.reverse.first
             disk_reservation = f_node['storage_devices'].filter{|d| d.key?('reservation')}.length > 0
-            gpu = f_node.key?('gpu_devices')             
-            warrantied = f_node['chassis'].key?('warranty_end') ? DateTime.parse(f_node['chassis']['warranty_end']) > DateTime.now : false
+            gpu = f_node.key?('gpu_devices') 
+            if ! f_node.key?('chassis')
+                puts "no chassis field for #{f_node['uid']}, setting warranty to false" 
+                warrantied = false
+            else
+                warrantied = f_node['chassis'].key?('warranty_end') ? DateTime.parse(f_node['chassis']['warranty_end']) > DateTime.now : false
+            end
 
             yaml_data[s_uid][c_uid] = {"queue" => queue, 
                                        "disk_reservation" => disk_reservation,
