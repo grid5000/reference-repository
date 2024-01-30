54b04ae762be68362f90b623427804cd41e48eab
Lucas Nussbaum <lucas.nussbaum@inria.fr>
2024-01-30 21:38:53 +0100

    [spec/input/] fix rubocop offenses

--- spec/input/add_gpu_computecapab_to_data.rb
+++ spec/input/add_gpu_computecapab_to_data.rb
@@ -13,14 +13,14 @@ Dir::glob('data*.json').each do |f|
   d = JSON::load(IO::read(f))
   d.each_pair do |k1, v1|
     next if k1 != 'sites'
-    v1.each_pair do |k2, v2|
+    v1.each_pair do |_k2, v2|
       v2.each_pair do |k3, v3|
         next if k3 != 'clusters'
-        v3.each_pair do |clusteruid, v4|
+        v3.each_pair do |_clusteruid, v4|
           v4.each_pair do |k5, v5|
             next if k5 != 'nodes'
-            v5.each_pair do |nodeuid, v6|
-              (v6['gpu_devices'] || {}).each_pair do |k7, v7|
+            v5.each_pair do |_nodeuid, v6|
+              (v6['gpu_devices'] || {}).each_pair do |_k7, v7|
                 if not v7.has_key?('compute_capability')
                   v7['compute_capability'] = '7.9'
                 end
